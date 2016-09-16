6ebde675ab84d26fc6c3e5820c2d74d176016463
Catherine Devlin <catherine.devlin@gsa.gov>
2016-09-16 12:22:47 -0400

    Preliminary BJS scraper

---
+++ scrape/scrape.py
@@ -0,0 +1,60 @@
+from robobrowser import RoboBrowser
+import json
+
+ALL = 'all values'
+
+class BJS(object):
+
+    url = 'http://www.bjs.gov/ucrdata/Search/Crime/Local/RunCrimeOneYearofData.cfm'
+
+    def __init__(self):
+        self.browser = RoboBrowser(history=True)
+        self.data = {}
+
+    def navigate(self, selections=[]):
+        """
+            selections: List of dicts,
+        """
+        self.browser.open(self.url)
+        self.form = self.browser.get_form('CFForm_1')
+        for sel in selections:
+            if 'submit' in sel:
+                which_submit = self.form.submit_fields[sel.pop('submit')]
+            else:
+                which_submit = None
+            for key in sel:
+                if sel[key] == ALL:
+                    self.form[key].value = self.form[key].options
+                else:
+                    self.form[key].value = sel[key]
+            self.browser.submit_form(self.form, which_submit)
+            self.form = self.browser.get_form('CFForm_1')
+
+
+bjs = BJS()
+bjs.navigate()
+states = bjs.form['StateId'].options
+for state in states:
+    print('State ' + state)
+    bjs.data[state] = {}
+    bjs.navigate([{'StateId': state}])
+    years = bjs.form['YearStart'].options
+    for year in years:
+        print('Year ' + year)
+        bjs.data[state][year] = {}
+        navigation = {'YearStart': year, 'CrimeCrossId': ALL, 'DataType': ALL, 'submit': 'NextPage'}
+        bjs.navigate([{'StateId': state}, navigation])
+
+        table = bjs.browser.find('table', title='Data table: crime, local-level, one year of data')
+        agency = None
+        for cell in table.find_all('td', headers=True):
+            if cell.attrs['headers'] == ['agency']:
+                agency = cell.text
+                print('agency ' + agency)
+                bjs.data[state][year][agency] = {}
+            else:
+                bjs.data[state][year][agency][cell.attrs['headers'][-1]] = cell.text
+
+
+with open('data.json', 'w') as outfile:
+    json.dump(bjs.data, outfile, indent=2)
