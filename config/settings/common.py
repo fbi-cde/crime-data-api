910d7be8fa0a10c5333fc45a32d3934676e36a5d
Colin Craig <colin.craig@gsa.gov>
2016-09-30 15:36:28 -0400

    reverted dev change

--- config/settings/common.py
+++ config/settings/common.py
@@ -100,7 +100,7 @@ MANAGERS = ADMINS
 # ------------------------------------------------------------------------------
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {
-    'default': env.db('DATABASE_URL', default='postgres:///fbi_ucr'),
+    'default': env.db('DATABASE_URL', default='postgres:///crime_data_api'),
 }
 DATABASES['default']['ATOMIC_REQUESTS'] = True
 
