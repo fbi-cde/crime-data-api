541641e8752bea92e992cda01fab922559bd23dd
Catherine Devlin <catherine.devlin@gsa.gov>
2016-10-17 09:17:44 -0400

    Use helper functions from incident endpoints

--- crime_data/resources/incidents.py
+++ crime_data/resources/incidents.py
@@ -5,6 +5,7 @@ from flask import request
 from flask_login import login_required
 #from webservices.common.views import ApiResource
 from flask_restful import Resource, fields, marshal_with, reqparse
+from .helpers import add_standard_arguments, verify_api_key
 
 from crime_data.common import models
 # from webservices import args
@@ -40,25 +41,19 @@ FIELDS = {
 }
 
 parser = reqparse.RequestParser()
-if os.getenv('VCAP_APPLICATION'):
-    parser.add_argument('api_key', required=True, help='Get from Catherine')
 parser.add_argument('crime_against')
 parser.add_argument('offense_code')
 parser.add_argument('offense_name')
 parser.add_argument('offense_category_name')
 parser.add_argument('location_code')
 parser.add_argument('location_name')
-parser.add_argument('page', type=int, default=1)
-parser.add_argument('page_size', type=int, default=10)
-
+add_standard_arguments(parser)
 
 class IncidentsList(Resource):
     @marshal_with(FIELDS)
     def get(self):
         args = parser.parse_args()
-        if os.getenv('VCAP_APPLICATION'):
-            if args['api_key'] != os.getenv('API_KEY'):
-                return ({'message': 'Ask Catherine for API key'}, 401)
+        verify_api_key(args)
         result = models.NibrsIncident.query
         if args['offense_code']:
             result = result.join(models.NibrsOffense). \
@@ -73,9 +68,7 @@ class IncidentsDetail(Resource):
     @marshal_with(FIELDS)
     def get(self, nbr):
         args = parser.parse_args()
-        if os.getenv('VCAP_APPLICATION'):
-            if args['api_key'] != os.getenv('API_KEY'):
-                return ({'message': 'Ask Catherine for API key'}, 401)
+        verify_api_key(args)
         incident = models.NibrsIncident.query.filter_by(
             incident_number=nbr).first()
         return incident
