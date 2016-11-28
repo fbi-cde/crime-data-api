bdf5049132a4b170804e57ccbacb81951176e5e7
Jacob Harris <jacob.harris@gsa.gov>
2016-11-28 16:53:56 -0500

    Decided to use pytest parametrize in codes test

--- tests/functional/test_codes.py
+++ tests/functional/test_codes.py
@@ -3,10 +3,11 @@
 
 See: http://webtest.readthedocs.org/
 """
+import pytest
 from crime_data.resources.codes import CodeReferenceList
 
 class TestCodesEndpoint:
-    def test_codes_endpoints_exists(self, user, testapp):
-        for code in CodeReferenceList.models:
-            res = testapp.get('/codes/{0}'.format(code))
-            assert res.status_code == 200
+    @pytest.mark.parametrize("table", CodeReferenceList.models)
+    def test_codes_endpoint_exists(self, user, testapp, table):
+        res = testapp.get('/codes/{0}'.format(table))
+        assert res.status_code == 200
