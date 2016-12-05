9b85c43fab04bbc2e79fbb4edbcc74dc07f78381
Jacob Harris <jacob.harris@gsa.gov>
2016-12-05 10:42:17 -0500

    Move codes lookup tables out of class

--- tests/functional/test_codes.py
+++ tests/functional/test_codes.py
@@ -4,10 +4,10 @@
 See: http://webtest.readthedocs.org/
 """
 import pytest
-from crime_data.resources.codes import CodeReferenceList
+from crime_data.resources.codes import CodeReferenceList, CODE_MODELS
 
 class TestCodesEndpoint:
-    @pytest.mark.parametrize("table", CodeReferenceList.models)
+    @pytest.mark.parametrize("table", CODE_MODELS)
     def test_codes_endpoint_exists(self, user, testapp, table):
         res = testapp.get('/codes/{0}'.format(table))
         assert res.status_code == 200
