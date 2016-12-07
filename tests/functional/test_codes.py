11275d5af2ac5955a3ed9fc3a13c68321b66b60e
Jacob Harris <jacob.harris@gsa.gov>
2016-12-07 12:39:12 -0500

    Remove a few more cases of the user testing fixture

--- tests/functional/test_codes.py
+++ tests/functional/test_codes.py
@@ -8,18 +8,18 @@ from crime_data.resources.codes import CODE_MODELS
 
 
 class TestCodesIndex:
-    def test_codes_index_exists(self, user, testapp):
+    def test_codes_index_exists(self, testapp):
         res = testapp.get('/codes')
         assert res.status_code == 200
 
 
 class TestCodesEndpoint:
     @pytest.mark.parametrize("table", CODE_MODELS)
-    def test_codes_endpoint_exists(self, user, testapp, table):
+    def test_codes_endpoint_exists(self, testapp, table):
         res = testapp.get('/codes/{0}'.format(table))
         assert res.status_code == 200
 
     @pytest.mark.parametrize("table", CODE_MODELS)
-    def test_codes_endpoint_csv(self, user, testapp, table):
+    def test_codes_endpoint_csv(self, testapp, table):
         res = testapp.get('/codes/{0}.csv'.format(table))
         assert res.status_code == 200
