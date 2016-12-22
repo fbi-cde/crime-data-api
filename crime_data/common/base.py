bb8194cc826cf0d4ef59c48ddd3b6e544d24b21e
Jacob Harris <jacob.harris@gsa.gov>
2016-12-21 21:05:20 -0500

    Setup New Relic monitoring for the API
    I also used this opportunity to extract the functionality for looking
    up credentials in a VCAP_SERVICES to a few helper methods

--- crime_data/common/base.py
+++ crime_data/common/base.py
@@ -13,6 +13,7 @@ from flask_apispec.views import MethodResource
 from flask_sqlalchemy import SignallingSession, SQLAlchemy
 from sqlalchemy import func, or_
 
+from crime_data.common.credentials import get_credential
 from crime_data.extensions import db
 
 session = db.session
@@ -399,16 +400,13 @@ class CdeResource(MethodResource):
         return output
 
     def verify_api_key(self, args):
-        if os.getenv('VCAP_SERVICES'):
-            service_env = json.loads(os.getenv('VCAP_SERVICES'))
-            cups_name = 'crime-data-api-creds'
-            creds = [
-                u['credentials'] for u in service_env['user-provided']
-                if 'credentials' in u
-            ]
-            key = creds[0]['API_KEY']
+        try:
+            key = get_credential('API_KEY')
             if args.get('api_key') != key:
                 abort(401, message='Use correct `api_key` argument')
+        except KeyError:
+            if os.getenv('VCAP_SERVICES'):
+                abort(500, message='Unable to verify api_key at this time')
 
     def _parse_inequality_operator(self, k, v):
         """
