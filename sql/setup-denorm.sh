5ec23f8fd6ab6a1644e27e6bff69a5d8561c39fb
Colin Craig <colin.craig@gsa.gov>
2017-01-19 11:09:26 -0500

    fixed scripts

--- sql/setup-denorm.sh
+++ sql/setup-denorm.sh
@@ -1,10 +1,10 @@
 # Build Denormalized tables.
 psql $CRIME_DATA_API_DEV_DB_URL -f sql/denorm-offender.sql
-psql $CRIME_DATA_API_DEV_DB_URL -f sqldenorm-offenses.sql
+psql $CRIME_DATA_API_DEV_DB_URL -f sql/denorm-offenses.sql
 psql $CRIME_DATA_API_DEV_DB_URL -f sql/denorm-victims.sql
 
 # Setup Materialized views.
-psql $CRIME_DATA_API_DEV_DB_URL -f sq/mat-views/cargo-theft.sql
+psql $CRIME_DATA_API_DEV_DB_URL -f sql/mat-views/cargo-theft.sql
 psql $CRIME_DATA_API_DEV_DB_URL -f sql/mat-views/hate-crime.sql
 psql $CRIME_DATA_API_DEV_DB_URL -f sql/mat-views/offender.sql
 psql $CRIME_DATA_API_DEV_DB_URL -f sql/mat-views/victim.sql
