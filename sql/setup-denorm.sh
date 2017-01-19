b0953274f1de0149b863e35e878a2849312e055a
Colin Craig <colin.craig@gsa.gov>
2017-01-19 11:08:47 -0500

    fixed scripts

--- sql/setup-denorm.sh
+++ sql/setup-denorm.sh
@@ -1,11 +1,11 @@
 # Build Denormalized tables.
-psql $CRIME_DATA_API_DEV_DB_URL -f denorm-offender.sql
-psql $CRIME_DATA_API_DEV_DB_URL -f denorm-offenses.sql
-psql $CRIME_DATA_API_DEV_DB_URL -f denorm-victims.sql
+psql $CRIME_DATA_API_DEV_DB_URL -f sql/denorm-offender.sql
+psql $CRIME_DATA_API_DEV_DB_URL -f sqldenorm-offenses.sql
+psql $CRIME_DATA_API_DEV_DB_URL -f sql/denorm-victims.sql
 
 # Setup Materialized views.
-psql $CRIME_DATA_API_DEV_DB_URL -f mat-views/cargo-theft.sql
-psql $CRIME_DATA_API_DEV_DB_URL -f mat-views/hate-crime.sql
-psql $CRIME_DATA_API_DEV_DB_URL -f mat-views/offender.sql
-psql $CRIME_DATA_API_DEV_DB_URL -f mat-views/victim.sql
+psql $CRIME_DATA_API_DEV_DB_URL -f sq/mat-views/cargo-theft.sql
+psql $CRIME_DATA_API_DEV_DB_URL -f sql/mat-views/hate-crime.sql
+psql $CRIME_DATA_API_DEV_DB_URL -f sql/mat-views/offender.sql
+psql $CRIME_DATA_API_DEV_DB_URL -f sql/mat-views/victim.sql
 # psql $CRIME_DATA_API_DEV_DB_URL -f mat-views/offense.sql
