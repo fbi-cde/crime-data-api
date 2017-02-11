893e79288f9b88b46ea7940c024e6abd4189547b
Jacob Harris <jacob.harris@gsa.gov>
2017-02-10 20:58:33 -0500

    Disabling caching for the CdeRefState
    For some reason, it's sharing the cached object across all
    instances. Python, I don't understand you sometimes.

--- tests/unit/test_cdemodels.py
+++ tests/unit/test_cdemodels.py
@@ -124,6 +124,16 @@ class TestCdeRefState:
         #                         AND rm.data_year=1960 and ra.state_id=55)
         assert state.covered_population_for_year(test_year) == 177770
 
+    def test_participation_cache_is_not_global(self, app):
+        test_year = 1960
+        state1 = CdeRefState.get(abbr='NY').one()
+        state2 = CdeRefState.get(abbr='DE').one()
+        assert state1 != state2
+        assert state1.state_id != state2.state_id
+        p1 = state1.total_population_for_year(test_year)
+        p2 = state2.total_population_for_year(test_year)
+        assert p1 != p2
+
 
 class TestOffenseCountView:
     """Test the OffenseCountView"""
