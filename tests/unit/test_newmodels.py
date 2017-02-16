7caa6158d74d9d000b863af7cca08818de8d94f8
Jacob Harris <jacob.harris@gsa.gov>
2017-02-15 22:12:34 -0500

    Adding arson to the reta_month_offense_subcat_summary
    Now rolls up properly into the classification=Property groupings (the
    tests in test_newmodels are there to confirm it works)

--- tests/unit/test_newmodels.py
+++ tests/unit/test_newmodels.py
@@ -55,22 +55,135 @@ class TestArsonAdditionsToRetaMonthOffenseSubcatSummary:
         q = q.filter(RetaMonthOffenseSubcatSummary.year == None)
         q = q.filter(RetaMonthOffenseSubcatSummary.state == None)
         q = q.filter(RetaMonthOffenseSubcatSummary.offense_subcat_code == None).one()
-        assert q.reported == 4290
-        assert q.unfounded == 481
-        assert q.actual == 3809
-        assert q.cleared == 624
-        assert q.juvenile_cleared == 220
+        assert q.reported == 4293
+        assert q.unfounded == 482
+        assert q.actual == 3811
+        assert q.cleared == 625
+        assert q.juvenile_cleared == 221
 
-    def test_arson_fields_for_state_month_year_subcategory(self, app):
-      q = RetaMonthOffenseSubcatSummary.query
+    def test_arson_fields_for_state_month_year(self, app):
+        q = RetaMonthOffenseSubcatSummary.query
         q = q.filter(RetaMonthOffenseSubcatSummary.classification == 'Property')
         q = q.filter(RetaMonthOffenseSubcatSummary.offense == 'Arson')
+        q = q.filter(RetaMonthOffenseSubcatSummary.year == 1984)
+        q = q.filter(RetaMonthOffenseSubcatSummary.month == 1)
+        q = q.filter(RetaMonthOffenseSubcatSummary.state == 'VA')
+        q = q.filter(RetaMonthOffenseSubcatSummary.offense_subcat_code == None).one()
+        assert q.reported == 7
+        assert q.unfounded == 4
+        assert q.actual == 3
+        assert q.cleared == 0
+        assert q.juvenile_cleared == 0
+
+    # Test it is adding counts to Classification
+    def test_classification_for_month_and_year_and_state(self, app):
+        q = RetaMonthOffenseSubcatSummary.query
+        q = q.filter(RetaMonthOffenseSubcatSummary.classification == 'Property')
+        q = q.filter(RetaMonthOffenseSubcatSummary.offense == None)
+        q = q.filter(RetaMonthOffenseSubcatSummary.offense_category == None)
+        q = q.filter(RetaMonthOffenseSubcatSummary.year == 2014)
+        q = q.filter(RetaMonthOffenseSubcatSummary.month == 1)
+        q = q.filter(RetaMonthOffenseSubcatSummary.state == 'AK').one()
+        assert q.reported == 12
+        assert q.unfounded == 1
+        assert q.actual == 11
+        assert q.cleared == 3
+        assert q.juvenile_cleared == 1
+
+    def test_classification_for_month_and_year(self, app):
+        q = RetaMonthOffenseSubcatSummary.query
+        q = q.filter(RetaMonthOffenseSubcatSummary.classification == 'Property')
+        q = q.filter(RetaMonthOffenseSubcatSummary.offense == None)
+        q = q.filter(RetaMonthOffenseSubcatSummary.offense_category == None)
+        q = q.filter(RetaMonthOffenseSubcatSummary.year == 2014)
+        q = q.filter(RetaMonthOffenseSubcatSummary.month == 1)
+        q = q.filter(RetaMonthOffenseSubcatSummary.state == None).one()
+        assert q.reported == 12
+        assert q.unfounded == 1
+        assert q.actual == 11
+        assert q.cleared == 3
+        assert q.juvenile_cleared == 1
+
+    def test_classification_for_month_and_year_and_other_state(self, app):
+        q = RetaMonthOffenseSubcatSummary.query
+        q = q.filter(RetaMonthOffenseSubcatSummary.classification == 'Property')
+        q = q.filter(RetaMonthOffenseSubcatSummary.offense == None)
+        q = q.filter(RetaMonthOffenseSubcatSummary.offense_category == None)
+        q = q.filter(RetaMonthOffenseSubcatSummary.year == 2014)
+        q = q.filter(RetaMonthOffenseSubcatSummary.month == 1)
+        q = q.filter(RetaMonthOffenseSubcatSummary.state == 'PA').one()
+        assert q.reported == 0
+        assert q.unfounded == 0
+        assert q.actual == 0
+        assert q.cleared == 0
+        assert q.juvenile_cleared == 0
+
+    def test_classification_for_month_and_state(self, app):
+        q = RetaMonthOffenseSubcatSummary.query
+        q = q.filter(RetaMonthOffenseSubcatSummary.classification == 'Property')
+        q = q.filter(RetaMonthOffenseSubcatSummary.offense == None)
+        q = q.filter(RetaMonthOffenseSubcatSummary.offense_category == None)
+        q = q.filter(RetaMonthOffenseSubcatSummary.month == 1)
+        q = q.filter(RetaMonthOffenseSubcatSummary.year == None)
+        q = q.filter(RetaMonthOffenseSubcatSummary.state == 'AK').one()
+        assert q.reported == 19
+        assert q.unfounded == 1
+        assert q.actual == 18
+        assert q.cleared == 4
+        assert q.juvenile_cleared == 1
+
+    def test_classification_for_year_and_state(self, app):
+        q = RetaMonthOffenseSubcatSummary.query
+        q = q.filter(RetaMonthOffenseSubcatSummary.classification == 'Property')
+        q = q.filter(RetaMonthOffenseSubcatSummary.offense == None)
+        q = q.filter(RetaMonthOffenseSubcatSummary.offense_category == None)
+        q = q.filter(RetaMonthOffenseSubcatSummary.month == None)
+        q = q.filter(RetaMonthOffenseSubcatSummary.year == 2014)
+        q = q.filter(RetaMonthOffenseSubcatSummary.state == 'AK').one()
+        assert q.reported == 12
+        assert q.unfounded == 1
+        assert q.actual == 11
+        assert q.cleared == 3
+        assert q.juvenile_cleared == 1
+
+    def test_classification_for_state(self, app):
+        q = RetaMonthOffenseSubcatSummary.query
+        q = q.filter(RetaMonthOffenseSubcatSummary.classification == 'Property')
+        q = q.filter(RetaMonthOffenseSubcatSummary.offense == None)
+        q = q.filter(RetaMonthOffenseSubcatSummary.offense_category == None)
+        q = q.filter(RetaMonthOffenseSubcatSummary.month == None)
+        q = q.filter(RetaMonthOffenseSubcatSummary.year == None)
+        q = q.filter(RetaMonthOffenseSubcatSummary.state == 'AK').one()
+        assert q.reported == 27
+        assert q.unfounded == 4
+        assert q.actual == 23
+        assert q.cleared == 7
+        assert q.juvenile_cleared == 2
+
+    def test_classification_for_year(self, app):
+        q = RetaMonthOffenseSubcatSummary.query
+        q = q.filter(RetaMonthOffenseSubcatSummary.classification == 'Property')
+        q = q.filter(RetaMonthOffenseSubcatSummary.offense == None)
+        q = q.filter(RetaMonthOffenseSubcatSummary.offense_category == None)
+        q = q.filter(RetaMonthOffenseSubcatSummary.month == None)
+        q = q.filter(RetaMonthOffenseSubcatSummary.state == None)
+        q = q.filter(RetaMonthOffenseSubcatSummary.year == 2014).one()
+        assert q.reported == 12
+        assert q.unfounded == 1
+        assert q.actual == 11
+        assert q.cleared == 3
+        assert q.juvenile_cleared == 1
+
+    def test_classification_overall(self, app):
+        q = RetaMonthOffenseSubcatSummary.query
+        q = q.filter(RetaMonthOffenseSubcatSummary.classification == 'Property')
+        q = q.filter(RetaMonthOffenseSubcatSummary.offense == None)
         q = q.filter(RetaMonthOffenseSubcatSummary.month == None)
         q = q.filter(RetaMonthOffenseSubcatSummary.year == None)
         q = q.filter(RetaMonthOffenseSubcatSummary.state == None)
-        q = q.filter(RetaMonthOffenseSubcatSummary.offense_subcat_code == None).one()
-        assert q.reported == 4290
-        assert q.unfounded == 481
-        assert q.actual == 3809
-        assert q.cleared == 624
-        assert q.juvenile_cleared == 220
+        q = q.filter(RetaMonthOffenseSubcatSummary.offense_category == None).one()
+        assert q.reported == 4372
+        assert q.unfounded == 482
+        assert q.actual == 3890
+        assert q.cleared == 626
+        assert q.juvenile_cleared == 221
