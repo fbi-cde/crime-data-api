7288f89bf64b95ddb886f6a0aff65f1fe1f1a522
Jacob Harris <jacob.harris@gsa.gov>
2017-03-23 09:57:55 -0400

    Adding a path for a single count view endpoint

--- tests/functional/test_victims.py
+++ tests/functional/test_victims.py
@@ -5,31 +5,36 @@ See: http://webtest.readthedocs.org/
 """
 import pytest
 from crime_data.common.cdemodels import VictimCountView
+from flex.core import validate_api_call
 
 class TestVictimsEndpoint:
 
     @pytest.mark.parametrize('variable', VictimCountView.VARIABLES)
-    def test_state_endpoint_count(self, testapp, variable):
+    def test_state_endpoint_count(self, testapp, swagger, variable):
         url = '/victims/count/states/3/{}?year=2014'.format(variable)
         res = testapp.get(url)
         assert res.status_code == 200
         assert 'pagination' in res.json
         for r in res.json['results']:
             assert 'count' in r
+        validate_api_call(swagger, raw_request=res.request, raw_response=res)
 
-    def test_state_endpoint_count_with_postal_code(self, testapp):
+
+    def test_state_endpoint_count_with_postal_code(self, testapp, swagger):
         url = '/victims/count/states/AR/race_code?year=2014'
         res = testapp.get(url)
         assert res.status_code == 200
         assert 'pagination' in res.json
         for r in res.json['results']:
             assert 'count' in r
+        validate_api_call(swagger, raw_request=res.request, raw_response=res)
 
     @pytest.mark.parametrize('variable', VictimCountView.VARIABLES)
-    def test_national_endpoint_count(self, testapp, variable):
+    def test_national_endpoint_count(self, testapp, swagger, variable):
         url = '/victims/count/national/{}?year=2014'.format(variable)
         res = testapp.get(url)
         assert res.status_code == 200
         assert 'pagination' in res.json
         for r in res.json['results']:
             assert 'count' in r
+        validate_api_call(swagger, raw_request=res.request, raw_response=res)
