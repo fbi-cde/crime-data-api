cc53da649feecef1129975d8a1df85eeb140a43e
Jacob Harris <jacob.harris@gsa.gov>
2017-03-24 11:23:12 -0400

    Added Swagger endpoints for all the count views
    Still experimenting with how to tag things and describe documentation.

--- tests/functional/test_offense_cargo_theft.py
+++ tests/functional/test_offense_cargo_theft.py
@@ -6,30 +6,34 @@ See: http://webtest.readthedocs.org/
 import pytest
 from crime_data.common.cdemodels import OffenseCargoTheftCountView
 from crime_data.common.base import ExplorerOffenseMapping
+from flex.core import validate_api_call
 
 class TestVictimsEndpoint:
-    def test_state_endpoint_no_year_in_request(self, testapp):
+    def test_state_endpoint_no_year_in_request(self, testapp, swagger):
         res = testapp.get('/ct/count/states/3/prop_desc_name/offenses')
         assert 'pagination' in res.json
         assert res.status_code == 200
+        validate_api_call(swagger, raw_request=res.request, raw_response=res)
         for r in res.json['results']:
             assert 'count' in r
             assert 'recovered_value' in r
             assert 'stolen_value' in r
 
     @pytest.mark.parametrize('variable', OffenseCargoTheftCountView.VARIABLES)
-    def test_victims_offenses_endpoint_with_just_state_year(self, testapp, variable):
+    def test_victims_offenses_endpoint_with_just_state_year(self, testapp, swagger, variable):
         url = '/ct/count/states/43/{}/offenses?year=2014'.format(variable)
         res = testapp.get(url)
+        validate_api_call(swagger, raw_request=res.request, raw_response=res)
         assert 'pagination' in res.json
         for r in res.json['results']:
             assert 'count' in r
             assert 'stolen_value' in r
             assert 'recovered_value' in r
 
-    def test_victims_offenses_endpoint_with_postal_code(self, testapp):
+    def test_victims_offenses_endpoint_with_postal_code(self, testapp, swagger):
         url = '/ct/count/states/AR/prop_desc_name/offenses?year=2014'
         res = testapp.get(url)
+        validate_api_call(swagger, raw_request=res.request, raw_response=res)
         assert 'pagination' in res.json
         for r in res.json['results']:
             assert 'count' in r
@@ -37,9 +41,10 @@ class TestVictimsEndpoint:
             assert 'recovered_value' in r
 
     @pytest.mark.parametrize('variable', OffenseCargoTheftCountView.VARIABLES)
-    def test_victims_offenses_endpoint_with_state_year_offense(self, testapp, variable):
+    def test_victims_offenses_endpoint_with_state_year_offense(self, testapp, swagger, variable):
         url = '/ct/count/states/48/{}/offenses?offense_name=Robbery&year=2014'.format(variable)
         res = testapp.get(url)
+        validate_api_call(swagger, raw_request=res.request, raw_response=res)
         assert 'pagination' in res.json
         for r in res.json['results']:
             assert 'count' in r
@@ -48,9 +53,10 @@ class TestVictimsEndpoint:
 
     @pytest.mark.parametrize('variable', OffenseCargoTheftCountView.VARIABLES)
     @pytest.mark.parametrize('explorer_offense', ExplorerOffenseMapping.NIBRS_OFFENSE_MAPPING.keys())
-    def test_victims_offenses_endpoint_with_state_year_explorer_offense(self, testapp, variable, explorer_offense):
+    def test_victims_offenses_endpoint_with_state_year_explorer_offense(self, testapp, swagger, variable, explorer_offense):
         url = '/ct/count/states/48/{}/offenses?explorer_offense={}&year=2014'.format(variable, explorer_offense)
         res = testapp.get(url)
+        validate_api_call(swagger, raw_request=res.request, raw_response=res)
         assert 'pagination' in res.json
         for r in res.json['results']:
             assert 'count' in r
--- tests/functional/test_offense_hate_crime.py
+++ tests/functional/test_offense_hate_crime.py
@@ -6,50 +6,57 @@ See: http://webtest.readthedocs.org/
 import pytest
 from crime_data.common.cdemodels import OffenseHateCrimeCountView
 from crime_data.common.base import ExplorerOffenseMapping
+from flex.core import validate_api_call
 
 class TestOffendersOffensesEndpoint:
-    def test_state_endpoint_no_year_in_request(self, testapp):
+    def test_state_endpoint_no_year_in_request(self, testapp, swagger):
         res = testapp.get('/hc/count/states/3/bias_name/offenses')
         assert 'pagination' in res.json
         assert res.status_code == 200
+        validate_api_call(swagger, raw_request=res.request, raw_response=res)
         for r in res.json['results']:
             assert 'count' in r
 
-    def test_state_endpoint_no_year_in_request_with_postal_code(self, testapp):
+    def test_state_endpoint_no_year_in_request_with_postal_code(self, swagger, testapp):
         res = testapp.get('/hc/count/states/AR/bias_name/offenses')
         assert 'pagination' in res.json
         assert res.status_code == 200
+        validate_api_call(swagger, raw_request=res.request, raw_response=res)
         for r in res.json['results']:
             assert 'count' in r
 
     @pytest.mark.parametrize('variable', OffenseHateCrimeCountView.VARIABLES)
-    def test_victims_offenses_endpoint_with_just_state_year(self, testapp, variable):
+    def test_victims_offenses_endpoint_with_just_state_year(self, testapp, swagger, variable):
         url = '/hc/count/states/43/{}/offenses?year=2014'.format(variable)
         res = testapp.get(url)
+        validate_api_call(swagger, raw_request=res.request, raw_response=res)
         assert 'pagination' in res.json
         for r in res.json['results']:
             assert 'count' in r
 
-    def test_victims_offenses_endpoint_with_just_state_year_and_postal_code(self, testapp):
+    def test_victims_offenses_endpoint_with_just_state_year_and_postal_code(self, testapp, swagger):
         url = '/hc/count/states/NY/bias_name/offenses?year=2014'
         res = testapp.get(url)
+        validate_api_call(swagger, raw_request=res.request, raw_response=res)
         assert 'pagination' in res.json
         for r in res.json['results']:
             assert 'count' in r
 
     @pytest.mark.parametrize('variable', OffenseHateCrimeCountView.VARIABLES)
-    def test_victims_offenses_endpoint_with_state_year_offense(self, testapp, variable):
+    def test_victims_offenses_endpoint_with_state_year_offense(self, testapp, swagger, variable):
         url = '/hc/count/states/43/{}/offenses?offense_name=Aggravated+Assault&year=2014'.format(variable)
         res = testapp.get(url)
+        validate_api_call(swagger, raw_request=res.request, raw_response=res)
         assert 'pagination' in res.json
         for r in res.json['results']:
             assert 'count' in r
 
     @pytest.mark.parametrize('variable', OffenseHateCrimeCountView.VARIABLES)
     @pytest.mark.parametrize('explorer_offense', ExplorerOffenseMapping.NIBRS_OFFENSE_MAPPING.keys())
-    def test_victims_offenses_endpoint_with_state_year_offense(self, testapp, variable, explorer_offense):
+    def test_victims_offenses_endpoint_with_state_year_offense(self, testapp, swagger, variable, explorer_offense):
         url = '/hc/count/states/43/{}/offenses?explorer_offense={}&year=2014'.format(variable, explorer_offense)
         res = testapp.get(url)
+        validate_api_call(swagger, raw_request=res.request, raw_response=res)
         assert 'pagination' in res.json
         for r in res.json['results']:
             assert 'count' in r
