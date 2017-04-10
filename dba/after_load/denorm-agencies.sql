0e939e4b9b23c0ec48ba5298ee6576940c7d18b4
Jacob Harris <jacob.harris@gsa.gov>
2017-04-09 22:06:42 -0400

    Fix for multiple ref_agency_county in cde_agencies

--- dba/after_load/denorm-agencies.sql
+++ dba/after_load/denorm-agencies.sql
@@ -99,10 +99,11 @@ LEFT OUTER JOIN cde_annual_participation cap ON cap.agency_id=ra.agency_id AND c
 LEFT OUTER JOIN ref_submitting_agency rsa ON rsa.agency_id=ra.submitting_agency_id
 LEFT OUTER JOIN ref_state rss ON rss.state_id=rsa.state_id
 LEFT OUTER JOIN ref_agency_population rap ON rap.agency_id=ra.agency_id AND rap.data_year=y.current_year
-LEFT OUTER JOIN ref_agency_county rac ON rac.agency_id=ra.agency_id AND rac.data_year=y.current_year
+LEFT OUTER JOIN (SELECT DISTINCT ON (agency_id, data_year) agency_id, data_year, core_city_flag FROM ref_agency_county ORDER BY agency_id, data_year, core_city_flag DESC) rac ON rac.agency_id=ra.agency_id AND rac.data_year=y.current_year
 LEFT OUTER JOIN ref_population_group rpg ON rpg.population_group_id=rap.population_group_id
 LEFT OUTER JOIN ref_agency_covered_by_flat racp ON racp.agency_id=ra.agency_id AND racp.data_year=y.current_year
 LEFT OUTER JOIN ref_agency covering ON covering.agency_id=racp.covered_by_agency_id
 LEFT OUTER JOIN pe_employee_data ped ON ped.agency_id=ra.agency_id AND ped.data_year=pe.staffing_year;
+
 DROP TABLE IF EXISTS cde_agencies CASCADE;
 ALTER TABLE denorm_agencies_temp RENAME TO cde_agencies;
