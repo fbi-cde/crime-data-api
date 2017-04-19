5768b1c5d860146bb34efd00fc3f9bf419cec32c
Jacob Harris <jacob.harris@gsa.gov>
2017-04-19 13:40:40 -0400

    Added new API endpoints for estimated data

--- dba/after_load/ext/estimated.sql
+++ dba/after_load/ext/estimated.sql
@@ -1,38 +1,67 @@
 -- You will have to run each step of this manually on the server
-DROP TABLE IF EXISTS reta_estimated_temp;
+DROP TABLE IF EXISTS reta_estimated_csv;
 
-CREATE TABLE reta_estimated_temp
+CREATE TABLE reta_estimated_csv
 (
+year smallint NOT NULL,
+state_abbr character varying(2),
+population bigint,
+violent_crime bigint,
+homicide bigint,
+rape_legacy bigint,
+rape_revised bigint,
+robbery bigint,
+aggravated_assault bigint,
+property_crime bigint,
+burglary bigint,
+larceny bigint,
+motor_vehicle_theft bigint,
+caveats text
+);
+
+\COPY reta_estimated_csv FROM 'estimated_1995_2015.csv' DELIMITER ',' HEADER CSV;
+
+DROP TABLE IF EXISTS reta_estimated;
+
+CREATE TABLE reta_estimated
+(
+  estimate_id SERIAL PRIMARY KEY,
   year smallint NOT NULL,
-  state_id bigint,
+  state_id smallint REFERENCES ref_state (state_id),
   state_abbr character varying(2),
   population bigint,
   violent_crime bigint,
   homicide bigint,
   rape_legacy bigint,
-  rape bigint,
+  rape_revised bigint,
   robbery bigint,
   aggravated_assault bigint,
   property_crime bigint,
   burglary bigint,
   larceny bigint,
   motor_vehicle_theft bigint,
-  caveats text
-)
-WITH (
-     OIDS = FALSE
+  caveats text,
+  UNIQUE (year, state_id)
 );
 
-\COPY reta_estimated_temp FROM 'estimated_1995_2015.csv' DELIMITER ',' HEADER CSV;
+INSERT INTO reta_estimated(year, state_id, state_abbr, population, violent_crime, homicide, rape_legacy, rape_revised, robbery, aggravated_assault, property_crime, burglary, larceny, motor_vehicle_theft, caveats)
+SELECT
+ret.year,
+rs.state_id,
+ret.state_abbr,
+ret.population,
+ret.violent_crime,
+ret.homicide,
+ret.rape_legacy,
+ret.rape_revised,
+ret.robbery,
+ret.aggravated_assault,
+ret.property_crime,
+ret.burglary,
+ret.larceny,
+ret.motor_vehicle_theft,
+ret.caveats
+FROM reta_estimated_csv ret
+LEFT OUTER JOIN ref_state rs ON rs.state_postal_abbr=ret.state_abbr;
 
-UPDATE reta_estimated_temp
-SET state_id=ref_state.state_id FROM ref_state WHERE ref_state.state_postal_abbr=reta_estimated_temp.state_abbr;
-
-ALTER TABLE ONLY reta_estimated_temp
-ADD CONSTRAINT reta_estimated_pkey PRIMARY KEY (year, state_id);
-
-ALTER TABLE ONLY reta_estimated_temp
-ADD CONSTRAINT reta_estimated_state_fk FOREIGN KEY (state_id) REFERENCES ref_state(state_id);
-
-DROP TABLE IF EXISTS reta_estimated CASCADE;
-ALTER TABLE reta_estimated_temp RENAME TO reta_estimated;
+DROP TABLE reta_estimated_csv;
