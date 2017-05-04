d73f1be45fde7f127bfd035a74ec7078a09ba9d0
Jacob Harris <jacob.harris@gsa.gov>
2017-05-04 13:18:21 -0400

    Fix tests to match test data from Rhode Island

--- tests/unit/test_newmodels.py
+++ tests/unit/test_newmodels.py
@@ -6,54 +6,88 @@ from crime_data.common.newmodels import (RetaMonthOffenseSubcatSummary,
                                          CdeAgency)
 import pytest
 
+## Check these tests
 class TestAgencyParticipation:
+    def test_for_agency_not_reporting(self, app):
+        q = AgencyParticipation.query
+        q = q.filter(AgencyParticipation.year == 2014)
+        q = q.filter(AgencyParticipation.agency_id == 17380).one()
+        assert q.reported == 0
+        assert q.months_reported == 0
+        assert q.nibrs_reported == 0
+        assert q.nibrs_months_reported == 0
+
+    def test_for_agency_covered_by_another(self, app):
+        q = AgencyParticipation.query
+        q = q.filter(AgencyParticipation.year == 2014)
+        q = q.filter(AgencyParticipation.agency_id == 17391).one()
+        assert q.reported == 0
+        assert q.months_reported == 0
+        assert q.nibrs_reported == 0
+        assert q.nibrs_months_reported == 0
+        assert q.covered == 1
+        assert q.participated == 1
+        assert q.nibrs_participated == 1
+
     def test_for_agency_in_nibrs_month(self, app):
         q = AgencyParticipation.query
-        q = q.filter(AgencyAnnualParticipation.data_year == 2014)
-        q = q.filter(AgencyAnnualParticipation.agency_id == 17381).one()
+        q = q.filter(AgencyParticipation.year == 2014)
+        q = q.filter(AgencyParticipation.agency_id == 17381).one()
         assert q.reported == 1
         assert q.months_reported == 12
-        assert q.reported_nibrs == 1
-        assert q.months_reported_nibrs == 12
+        assert q.nibrs_reported == 1
+        assert q.nibrs_months_reported == 12
 
     def test_for_agency_not_in_nibrs_month(self, app):
         q = AgencyParticipation.query
-        q = q.filter(AgencyAnnualParticipation.data_year == 2014)
-        q = q.filter(AgencyAnnualParticipation.agency_id == 17427).one()
+        q = q.filter(AgencyParticipation.year == 2014)
+        q = q.filter(AgencyParticipation.agency_id == 17427).one()
         assert q.reported == 1
         assert q.months_reported == 12
-        assert q.reported_nibrs == 0
-        assert q.months_reported_nibrs == 0
+        assert q.nibrs_reported == 0
+        assert q.nibrs_months_reported == 0
 
 
 class TestParticipationRate:
     def test_for_state_in_year(self, app):
         q = ParticipationRate.query
-        q = q.filter(ParticipationRate.data_year == 2014)
+        q = q.filter(ParticipationRate.year == 2014)
         q = q.filter(ParticipationRate.state_id == 44).one()
-        assert q.data_year == 2014
+        assert q.year == 2014
         assert q.state_id == 44
-        assert q.total_agencies == 56
-        assert q.reporting_agencies == 52
-        assert q.reporting_rate == pytest.approx(0.928571429)
-        assert q.nibrs_reporting_agencies == 50
-        assert q.nibrs_reporting_rate == pytest.approx(0.892857143)
+        assert q.total_agencies == 62
+        assert q.participating_agencies == 54
+        assert q.participation_rate == pytest.approx(0.870967742)
+        assert q.nibrs_participating_agencies == 52
+        assert q.nibrs_participation_rate == pytest.approx(0.838709677)
 
+    def test_for_county_in_year(self, app):
+        pass
+        q = ParticipationRate.query
+        q = q.filter(ParticipationRate.year == 2014)
+        q = q.filter(ParticipationRate.state_id == 44).one()
+        assert q.year == 2014
+        assert q.state_id == 44
+        assert q.total_agencies == 62
+        assert q.participating_agencies == 54
+        assert q.participation_rate == pytest.approx(0.870967742)
+        assert q.nibrs_participating_agencies == 52
+        assert q.nibrs_participation_rate == pytest.approx(0.838709677)
 
     def test_for_year(self, app):
         q = ParticipationRate.query
-        q = q.filter(ParticipationRate.data_year == 2014)
+        q = q.filter(ParticipationRate.year == 2014)
         q = q.filter(ParticipationRate.state_id == None)
         q = q.filter(ParticipationRate.county_id == None).one()
-        assert q.data_year == 2014
+        assert q.year == 2014
         assert q.state_id is None
-        assert q.total_agencies == 58
-        assert q.reporting_agencies == 54
-        assert q.reporting_rate == pytest.approx(0.931034483)
-        assert q.nibrs_reporting_agencies == 50
-        assert q.nibrs_reporting_rate == pytest.approx(0.862068966)
+        assert q.total_agencies == 64
+        assert q.participating_agencies == 54
+        assert q.participation_rate == pytest.approx(0.84375)
+        assert q.nibrs_participating_agencies == 52
+        assert q.nibrs_participation_rate == pytest.approx(0.8125)
         assert q.total_population == 12142430
-        assert q.covered_population == 1323169
+        assert q.participating_population == 1055173
 
 
 class TestCdeAgencies:
