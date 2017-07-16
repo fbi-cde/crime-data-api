ef32d07f5347929bdfc648349bba7a0de7ad00be
Jacob Harris <jacob.harris@gsa.gov>
2017-07-16 08:53:50 -0400

    Juvenile and Adult crosstabs for ASR data

--- dba/after_load/asr_summaries.sql
+++ dba/after_load/asr_summaries.sql
@@ -146,6 +146,7 @@ FROM asr_age_suboffense_summary aass
 JOIN asr_offense_subcat aos ON aos.offense_subcat_id = aass.offense_subcat_id
 JOIN asr_offense ao ON ao.offense_id = aos.offense_id
 JOIN asr_age_range aar ON aar.age_range_id = aass.age_range_id
+WHERE aos.offense_subcat_id IN (11, 12, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 18, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280)
 GROUP BY GROUPING SETS(
 (aass.data_year),
 (aass.data_year, aar.juvenile_flag),
@@ -170,6 +171,7 @@ FROM asr_race_suboffense_summary aass
 JOIN asr_offense_subcat aos ON aos.offense_subcat_id = aass.offense_subcat_id
 JOIN asr_offense ao ON ao.offense_id = aos.offense_id
 JOIN ref_race rr ON rr.race_id = aass.race_id
+WHERE aos.offense_subcat_id IN (11, 12, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 18, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280)
 GROUP BY GROUPING SETS(
 (aass.data_year, race_code, race_desc),
 (aass.data_year, juvenile_flag, race_code, race_desc),
@@ -285,84 +287,460 @@ CREATE INDEX asr_state_offense_race_code_idx ON asr_state_offense_summary (state
 CREATE INDEX asr_state_offense_offense_code_idx ON asr_state_offense_summary (state_abbr, offense_code);
 CREATE INDEX asr_state_offense_offense_subcat_code_idx ON asr_state_offense_summary (state_abbr, offense_subcat_code);
 
----- DRUG ARRESTS
+---- JUVENILE CROSSTAB
 
-DROP TABLE If EXISTS asr_drug_crosstab;
-CREATE TABLE asr_drug_crosstab(
-id serial PRIMARY KEY,
-year smallint,
-state_abbr character(2),
-agencies bigint,
-population bigint,
-total_arrests bigint,
-total_manufacture bigint,
-opioid_manufacture bigint,
-marijuana_manufacture bigint,
-synthetic_manufacture bigint,
-other_manufacture bigint,
-total_possess bigint,
-opioid_possess bigint,
-marijuana_possess bigint,
-synthetic_possess bigint,
-other_possess bigint,
-UNIQUE(year, state_abbr)
+DROP TABLE IF EXISTS asr_juvenile_crosstab;
+CREATE TABLE asr_juvenile_crosstab(
+  id serial PRIMARY KEY,
+  year smallint,
+  offense_code text,
+  offense_name text,
+  agencies bigint,
+  population bigint,
+  total_male bigint,
+  total_female bigint,
+  m_0_9 bigint,
+  m_10_12 bigint,
+  m_13_14 bigint,
+  m_15 bigint,
+  m_16 bigint,
+  m_17 bigint,
+  f_0_9 bigint,
+  f_10_12 bigint,
+  f_13_14 bigint,
+  f_15 bigint,
+  f_16 bigint,
+  f_17 bigint,
+  race_agencies bigint,
+  race_population bigint,
+  white bigint,
+  black bigint,
+  asian bigint,
+  asian_pacific_islander bigint,
+  american_indian bigint,
+  UNIQUE(year, state_abbr)
 );
 
-DROP TABLE IF EXISTS asr_drug_labels;
-CREATE TEMPORARY TABLE asr_drug_labels (
+DROP TABLE IF EXISTS asr_age_labels;
+CREATE TEMPORARY TABLE asr_age_labels (
+code text NOT NULL,
+label text NOT NULL,
+juvenile_flag character(1) NOT NULL
+);
+
+INSERT INTO asr_age_labels VALUES
+  ('M_0_9', 'm_0_9', 'Y'),
+  ('M_10_12', 'm_10_12', 'Y'),
+  ('M_13_14', 'm_13_14', 'Y'),
+  ('M_15', 'm_15', 'Y'),
+  ('M_16', 'm_16', 'Y'),
+  ('M_17', 'm_17', 'Y'),
+  ('M_18', 'm_18', 'N'),
+  ('M_19', 'm_19', 'N'),
+  ('M_20', 'm_20', 'N'),
+  ('M_21', 'm_21', 'N'),
+  ('M_22', 'm_22', 'N'),
+  ('M_23', 'm_23', 'N'),
+  ('M_24', 'm_24', 'N'),
+  ('M_25_29', 'm_25_29', 'N'),
+  ('M_30_34', 'm_30_34', 'N'),
+  ('M_35_39', 'm_35_39', 'N'),
+  ('M_40_44', 'm_40_44', 'N'),
+  ('M_45_49', 'm_45_49', 'N'),
+  ('M_50_54', 'm_50_54', 'N'),
+  ('M_55_59', 'm_55_59', 'N'),
+  ('M_60_64', 'm_60_64', 'N'),
+  ('M_65P', 'm_65p', 'N'),
+  ('F_0_9', 'f_0_9', 'Y'),
+  ('F_10_12', 'f_10_12', 'Y'),
+  ('F_13_14', 'f_13_14', 'Y'),
+  ('F_15', 'f_15', 'Y'),
+  ('F_16', 'f_16', 'Y'),
+  ('F_17', 'f_17', 'Y'),
+  ('F_18', 'f_18', 'N'),
+  ('F_19', 'f_19', 'N'),
+  ('F_20', 'f_20', 'N'),
+  ('F_21', 'f_21', 'N'),
+  ('F_22', 'f_22', 'N'),
+  ('F_23', 'f_23', 'N'),
+  ('F_24', 'f_24', 'N'),
+  ('F_25_29', 'f_25_29', 'N'),
+  ('F_30_34', 'f_30_34', 'N'),
+  ('F_35_39', 'f_35_39', 'N'),
+  ('F_40_44', 'f_40_44', 'N'),
+  ('F_45_49', 'f_45_49', 'N'),
+  ('F_50_54', 'f_50_54', 'N'),
+  ('F_55_59', 'f_55_59', 'N'),
+  ('F_60_64', 'f_60_64', 'N'),
+  ('F_65P', 'f_65p', 'N');
+
+
+DROP TABLE IF EXISTS asr_juvenile_age_crosstab;
+CREATE TEMPORARY TABLE asr_juvenile_age_crosstab(
+   id serial PRIMARY KEY,
+   year smallint,
+   offense_code text,
+   f_0_9 bigint,
+   f_10_12 bigint,
+   f_13_14 bigint,
+   f_15 bigint,
+   f_16 bigint,
+   f_17 bigint,
+   m_0_9 bigint,
+   m_10_12 bigint,
+   m_13_14 bigint,
+   m_15 bigint,
+   m_16 bigint,
+   m_17 bigint,
+   UNIQUE(year, offense_code)
+);
+
+DO
+$do$
+DECLARE
+offense_codes text[] := ARRAY['ASR_HOM', 'ASR_MAN', 'ASR_RPE', 'ASR_ROB', 'ASR_AST', 'ASR_BRG', 'ASR_LRC', 'ASR_MVT', 'ASR_AST_SMP', 'ASR_ARSON', 'ASR_FOR', 'ASR_FRD', 'ASR_EMB', 'ASR_STP', 'ASR_VAN', 'ASR_WEAP', 'ASR_PRS', 'ASR_SEX', 'ASR_DRG', 'ASR_GAM', 'ASR_FAM', 'ASR_DUI', 'ASR_LIQ', 'ASR_DRK', 'ASR_DIS', 'ASR_VAG', 'ASR_OTH', 'ASR_SUS', 'ASR_CUR'];
+oc text;
+BEGIN
+  FOREACH oc IN ARRAY offense_codes
+  LOOP
+  INSERT INTO asr_juvenile_age_crosstab(offense_code, year, f_0_9, f_10_12, f_13_14, f_15, f_16, f_17, m_0_9, m_10_12, m_13_14, m_15, m_16, m_17)
+  SELECT oc AS offense_code, ct.*
+    FROM CROSSTAB(
+  $$
+  SELECT year,
+  l.label,
+  arrest_count
+  FROM asr_offense_summary a
+  JOIN asr_age_labels l ON l.code = a.age_range_code
+  WHERE a.juvenile_flag = 'Y'
+  AND age_range_code IS NOT NULL
+  AND offense_code = '$$ || oc || $$'
+  AND offense_subcat_code IS NULL
+  ORDER by 1,2;
+  $$,
+  $$ select label from asr_age_labels where juvenile_flag='Y' order by label $$
+  ) as ct (
+  "year" smallint,
+  "f_0_9" bigint,
+  "f_10_12" bigint,
+  "f_13_14" bigint,
+  "f_15" bigint,
+  "f_16" bigint,
+  "f_17" bigint,
+  "m_0_9" bigint,
+  "m_10_12" bigint,
+  "m_13_14" bigint,
+  "m_15" bigint,
+  "m_16" bigint,
+  "m_17" bigint
+  );
+
+  END LOOP;
+END
+$do$;
+
+DROP TABLE IF EXISTS asr_race_labels;
+CREATE TEMPORARY TABLE asr_race_labels (
   code text,
   label text
 );
 
-INSERT INTO asr_drug_labels VALUES
-('ASR_DRG', 'total_arrests'),
-('ASR_DRG_MAN', 'total_manufacture'),
-('ASR_DRG_MAN_CKE', 'opioid_manufacture'),
-('ASR_DRG_MAN_MAR', 'marijuana_manufacture'),
-('ASR_DRG_MAN_SYN', 'synthetic_manufacture'),
-('ASR_DRG_MAN_OTH', 'other_manufacture'),
-('ASR_DRG_POS', 'total_possess'),
-('ASR_DRG_POS_CKE', 'opioid_possess'),
-('ASR_DRG_POS_MAR', 'marijuana_possess'),
-('ASR_DRG_POS_SYN', 'synthetic_possess'),
-('ASR_DRG_POS_OTH', 'other_possess');
+INSERT INTO asr_race_labels VALUES
+  ('W', 'white'),
+  ('B', 'black'),
+  ('A', 'asian'),
+  ('AP', 'asian_pacific_islander'),
+  ('I', 'american_indian');
 
-DROP TABLE IF EXISTS asr_drug_rollup;
-CREATE TEMPORARY TABLE asr_drug_rollup AS
-SELECT s.data_year AS year, s.state_id, d.label, SUM(arrest_count) AS arrest_count
-FROM asr_age_suboffense_summary s
-JOIN asr_offense_subcat aos ON aos.offense_subcat_id = s.offense_subcat_id
-JOIN asr_drug_labels d ON d.code = aos.offense_subcat_code
-GROUP BY GROUPING SETS(
-(s.data_year, s.state_id, d.label),
-(s.data_year, d.label)
+DROP TABLE IF EXISTS asr_juvenile_race_crosstab;
+CREATE TEMPORARY TABLE asr_juvenile_race_crosstab(
+   id serial PRIMARY KEY,
+   year smallint,
+   offense_code text,
+   white bigint,
+   black bigint,
+   asian bigint,
+   asian_pacific_islander bigint,
+   american_indian bigint,
+   UNIQUE(year, offense_code)
 );
 
-INSERT INTO asr_drug_crosstab(year, total_arrests, total_manufacture, opioid_manufacture, marijuana_manufacture, synthetic_manufacture, other_manufacture, total_possess, opioid_possess, marijuana_possess, synthetic_possess, other_possess)
-SELECT *
-FROM CROSSTAB(
-$$ SELECT year,
-label,
-arrest_count
-FROM asr_drug_rollup
-WHERE state_id IS NULL
-ORDER BY 1,2$$,
-$$ SELECT label from asr_drug_labels $$
-) AS ct (
-"year" smallint,
-"total_arrests" bigint,
-"total_manufacture" bigint,
-"opioid_manufacture" bigint,
-"marijuana_manufacture" bigint,
-"synthetic_manufacture" bigint,
-"other_manufacture" bigint,
-"total_possess" bigint,
-"opioid_possess" bigint,
-"marijuana_possess" bigint,
-"synthetic_possess" bigint,
-"other_possess" bigint
+DO
+$do$
+DECLARE
+offense_codes text[] := ARRAY['ASR_HOM', 'ASR_MAN', 'ASR_RPE', 'ASR_ROB', 'ASR_AST', 'ASR_BRG', 'ASR_LRC', 'ASR_MVT', 'ASR_AST_SMP', 'ASR_ARSON', 'ASR_FOR', 'ASR_FRD', 'ASR_EMB', 'ASR_STP', 'ASR_VAN', 'ASR_WEAP', 'ASR_PRS', 'ASR_SEX', 'ASR_DRG', 'ASR_GAM', 'ASR_FAM', 'ASR_DUI', 'ASR_LIQ', 'ASR_DRK', 'ASR_DIS', 'ASR_VAG', 'ASR_OTH', 'ASR_SUS', 'ASR_CUR'];
+oc text;
+BEGIN
+  FOREACH oc IN ARRAY offense_codes
+  LOOP
+  INSERT INTO asr_juvenile_race_crosstab(offense_code, year, white, black, asian, asian_pacific_islander, american_indian)
+  SELECT oc AS offense_code, ct.*
+    FROM CROSSTAB(
+  $$
+  SELECT year,
+  l.label,
+  arrest_count
+  FROM asr_offense_summary a
+  JOIN asr_race_labels l ON l.code = a.race_code
+  WHERE juvenile_flag = 'Y'
+  AND race_code IS NOT NULL
+  AND offense_code = '$$ || oc || $$'
+  AND offense_subcat_code IS NULL
+  ORDER by 1,2;
+  $$,
+  $$ select label from asr_race_labels order by label $$
+  ) as ct (
+  "year" smallint,
+  "white" bigint,
+  "black" bigint,
+  "asian" bigint,
+  "asian_pacific_islander" bigint,
+  "american_indian" bigint
+  );
+
+  END LOOP;
+END
+$do$;
+
+INSERT INTO asr_juvenile_crosstab(year, offense_code, offense_name, agencies, population, total_male, total_female, m_0_9, m_10_12, m_13_14, m_15, m_16, m_17, f_0_9, f_10_12, f_13_14, f_15, f_16, f_17, race_agencies, race_population, white, black, asian, asian_pacific_islander, american_indian)
+SELECT a.year, a.offense_code, o.offense_name, ap.agencies, ap.population, m_0_9+m_10_12+m_13_14+m_15+m_16+m_17 AS total_male, f_0_9+f_10_12+f_13_14+f_15+f_16+f_17 AS total_female, m_0_9, m_10_12, m_13_14, m_15, m_16, m_17, f_0_9, f_10_12, f_13_14, f_15, f_16, f_17, rp.agencies, rp.population, white, black, asian, asian_pacific_islander, american_indian
+FROM asr_juvenile_age_crosstab a
+JOIN asr_juvenile_race_crosstab r ON r.year = a.year AND r.offense_code = a.offense_code
+JOIN asr_offense o ON o.offense_code = a.offense_code
+JOIN asr_aas_populations ap ON ap.data_year = a.year and ap.state_abbr IS NULL
+JOIN asr_race_populations rp ON rp.data_year = a.year and rp.state_abbr IS NULL;
+
+------------  ADULT CROSSTAB
+
+DROP TABLE IF EXISTS asr_adult_crosstab;
+CREATE TABLE asr_adult_crosstab(
+  id serial PRIMARY KEY,
+  year smallint,
+  offense_code text,
+  offense_name text,
+  agencies bigint,
+  population bigint,
+  total_male bigint,
+  total_female bigint,
+  m_18 bigint,
+  m_19 bigint,
+  m_20 bigint,
+  m_21 bigint,
+  m_22 bigint,
+  m_23 bigint,
+  m_24 bigint,
+  m_25_29 bigint,
+  m_30_34 bigint,
+  m_35_39 bigint,
+  m_40_44 bigint,
+  m_45_49 bigint,
+  m_50_54 bigint,
+  m_55_59 bigint,
+  m_60_64 bigint,
+  m_65p   bigint,
+  f_18 bigint,
+  f_19 bigint,
+  f_20 bigint,
+  f_21 bigint,
+  f_22 bigint,
+  f_23 bigint,
+  f_24 bigint,
+  f_25_29 bigint,
+  f_30_34 bigint,
+  f_35_39 bigint,
+  f_40_44 bigint,
+  f_45_49 bigint,
+  f_50_54 bigint,
+  f_55_59 bigint,
+  f_60_64 bigint,
+  f_65p bigint,
+  race_agencies bigint,
+  race_population bigint,
+  white bigint,
+  black bigint,
+  asian bigint,
+  asian_pacific_islander bigint,
+  american_indian bigint,
+  UNIQUE(year, state_abbr)
 );
 
+DROP TABLE IF EXISTS asr_adult_age_crosstab;
+CREATE TEMPORARY TABLE asr_adult_age_crosstab(
+   id serial PRIMARY KEY,
+   year smallint,
+   offense_code text,
+   f_18 bigint,
+   f_19 bigint,
+   f_20 bigint,
+   f_21 bigint,
+   f_22 bigint,
+   f_23 bigint,
+   f_24 bigint,
+   f_25_29 bigint,
+   f_30_34 bigint,
+   f_35_39 bigint,
+   f_40_44 bigint,
+   f_45_49 bigint,
+   f_50_54 bigint,
+   f_55_59 bigint,
+   f_60_64 bigint,
+   f_65p bigint,
+   m_18 bigint,
+   m_19 bigint,
+   m_20 bigint,
+   m_21 bigint,
+   m_22 bigint,
+   m_23 bigint,
+   m_24 bigint,
+   m_25_29 bigint,
+   m_30_34 bigint,
+   m_35_39 bigint,
+   m_40_44 bigint,
+   m_45_49 bigint,
+   m_50_54 bigint,
+   m_55_59 bigint,
+   m_60_64 bigint,
+   m_65p   bigint,
+   UNIQUE(year, offense_code)
+);
+
+DO
+$do$
+DECLARE
+offense_codes text[] := ARRAY['ASR_HOM', 'ASR_MAN', 'ASR_RPE', 'ASR_ROB', 'ASR_AST', 'ASR_BRG', 'ASR_LRC', 'ASR_MVT', 'ASR_AST_SMP', 'ASR_ARSON', 'ASR_FOR', 'ASR_FRD', 'ASR_EMB', 'ASR_STP', 'ASR_VAN', 'ASR_WEAP', 'ASR_PRS', 'ASR_SEX', 'ASR_DRG', 'ASR_GAM', 'ASR_FAM', 'ASR_DUI', 'ASR_LIQ', 'ASR_DRK', 'ASR_DIS', 'ASR_VAG', 'ASR_OTH', 'ASR_SUS', 'ASR_CUR'];
+oc text;
+BEGIN
+  FOREACH oc IN ARRAY offense_codes
+  LOOP
+  INSERT INTO asr_adult_age_crosstab(offense_code, year, f_18, f_19, f_20, f_21, f_22, f_23, f_24, f_25_29, f_30_34, f_35_39, f_40_44, f_45_49, f_50_54, f_55_59, f_60_64, f_65p, m_18, m_19, m_20, m_21, m_22, m_23, m_24, m_25_29, m_30_34, m_35_39, m_40_44, m_45_49, m_50_54, m_55_59, m_60_64, m_65p)
+  SELECT oc AS offense_code, ct.*
+    FROM CROSSTAB(
+  $$
+  SELECT year,
+  l.label,
+  arrest_count
+  FROM asr_offense_summary a
+  JOIN asr_age_labels l ON l.code = a.age_range_code
+  WHERE a.juvenile_flag = 'N'
+  AND age_range_code IS NOT NULL
+  AND offense_code = '$$ || oc || $$'
+  AND offense_subcat_code IS NULL
+  ORDER by 1,2;
+  $$,
+  $$ SELECT label from asr_age_labels where juvenile_flag = 'N' order by label $$
+  ) as ct (
+  "year" smallint,
+  "f_18" bigint,
+  "f_19" bigint,
+  "f_20" bigint,
+  "f_21" bigint,
+  "f_22" bigint,
+  "f_23" bigint,
+  "f_24" bigint,
+  "f_25_29" bigint,
+  "f_30_34" bigint,
+  "f_35_39" bigint,
+  "f_40_44" bigint,
+  "f_45_49" bigint,
+  "f_50_54" bigint,
+  "f_55_59" bigint,
+  "f_60_64" bigint,
+  "f_65p"   bigint,
+  "m_18" bigint,
+  "m_19" bigint,
+  "m_20" bigint,
+  "m_21" bigint,
+  "m_22" bigint,
+  "m_23" bigint,
+  "m_24" bigint,
+  "m_25_29" bigint,
+  "m_30_34" bigint,
+  "m_35_39" bigint,
+  "m_40_44" bigint,
+  "m_45_49" bigint,
+  "m_50_54" bigint,
+  "m_55_59" bigint,
+  "m_60_64" bigint,
+  "m_65p"   bigint
+  );
+
+  END LOOP;
+END
+$do$;
+
+DROP TABLE IF EXISTS asr_race_labels;
+CREATE TEMPORARY TABLE asr_race_labels (
+  code text,
+  label text
+);
+
+INSERT INTO asr_race_labels VALUES
+  ('W', 'white'),
+  ('B', 'black'),
+  ('A', 'asian'),
+  ('AP', 'asian_pacific_islander'),
+  ('I', 'american_indian');
+
+DROP TABLE IF EXISTS asr_adult_race_crosstab;
+CREATE TEMPORARY TABLE asr_adult_race_crosstab(
+   id serial PRIMARY KEY,
+   year smallint,
+   offense_code text,
+   white bigint,
+   black bigint,
+   asian bigint,
+   asian_pacific_islander bigint,
+   american_indian bigint,
+   UNIQUE(year, offense_code)
+);
+
+DO
+$do$
+DECLARE
+offense_codes text[] := ARRAY['ASR_HOM', 'ASR_MAN', 'ASR_RPE', 'ASR_ROB', 'ASR_AST', 'ASR_BRG', 'ASR_LRC', 'ASR_MVT', 'ASR_AST_SMP', 'ASR_ARSON', 'ASR_FOR', 'ASR_FRD', 'ASR_EMB', 'ASR_STP', 'ASR_VAN', 'ASR_WEAP', 'ASR_PRS', 'ASR_SEX', 'ASR_DRG', 'ASR_GAM', 'ASR_FAM', 'ASR_DUI', 'ASR_LIQ', 'ASR_DRK', 'ASR_DIS', 'ASR_VAG', 'ASR_OTH', 'ASR_SUS', 'ASR_CUR'];
+oc text;
+BEGIN
+  FOREACH oc IN ARRAY offense_codes
+  LOOP
+  INSERT INTO asr_adult_race_crosstab(offense_code, year, white, black, asian, asian_pacific_islander, american_indian)
+  SELECT oc AS offense_code, ct.*
+    FROM CROSSTAB(
+  $$
+  SELECT year,
+  l.label,
+  arrest_count
+  FROM asr_offense_summary a
+  JOIN asr_race_labels l ON l.code = a.race_code
+  WHERE juvenile_flag = 'N'
+  AND race_code IS NOT NULL
+  AND offense_code = '$$ || oc || $$'
+  AND offense_subcat_code IS NULL
+  ORDER by 1,2;
+  $$,
+  $$ select label from asr_race_labels ORDER by label $$
+  ) as ct (
+  "year" smallint,
+  "white" bigint,
+  "black" bigint,
+  "asian" bigint,
+  "asian_pacific_islander" bigint,
+  "american_indian" bigint
+  );
+
+  END LOOP;
+END
+$do$;
+
+INSERT INTO asr_adult_crosstab(year, offense_code, offense_name, agencies, population, total_female, total_male, f_18, f_19, f_20, f_21, f_22, f_23, f_24, f_25_29, f_30_34, f_35_39, f_40_44, f_45_49, f_50_54, f_55_59, f_60_64, f_65p, m_18, m_19, m_20, m_21, m_22, m_23, m_24, m_25_29, m_30_34, m_35_39, m_40_44, m_45_49, m_50_54, m_55_59, m_60_64, m_65p, race_agencies, race_population, white, black, asian, asian_pacific_islander, american_indian)
+SELECT a.year, a.offense_code, o.offense_name, ap.agencies, ap.population, f_18+f_19+f_20+f_21+f_22+f_23+f_24+f_25_29+f_30_34+f_35_39+f_40_44+f_45_49+f_50_54+f_55_59+f_60_64+f_65p AS total_female, m_18+m_19+m_20+m_21+m_22+m_23+m_24+m_25_29+m_30_34+m_35_39+m_40_44+m_45_49+m_50_54+m_55_59+m_60_64+m_65p AS total_male, f_18, f_19, f_20, f_21, f_22, f_23, f_24, f_25_29, f_30_34, f_35_39, f_40_44, f_45_49, f_50_54, f_55_59, f_60_64, f_65p, m_18, m_19, m_20, m_21, m_22, m_23, m_24, m_25_29, m_30_34, m_35_39, m_40_44, m_45_49, m_50_54, m_55_59, m_60_64, m_65p, rp.agencies, rp.population, white, black, asian, asian_pacific_islander, american_indian
+FROM asr_adult_age_crosstab a
+JOIN asr_adult_race_crosstab r ON r.year = a.year AND r.offense_code = a.offense_code
+JOIN asr_offense o ON o.offense_code = a.offense_code
+JOIN asr_aas_populations ap ON ap.data_year = a.year and ap.state_abbr IS NULL
+JOIN asr_race_populations rp ON rp.data_year = a.year and rp.state_abbr IS NULL;
+
+
+------------- DRUG CROSSTAB
 DO
 $do$
 DECLARE
