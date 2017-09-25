5d0741a79465b7a681799e3507530a90fbd7081d
Jacob Wentz <jpwentz@ws05.ucr.development.cjis.gov>
2017-09-25 14:28:03 -0400

    Update to update csv files

--- dba/update/build_upload_script_other.sh
+++ dba/update/build_upload_script_other.sh
@@ -480,80 +480,7 @@ CREATE TABLE pe_employee_data_temp (
 \COPY pe_employee_data_temp (agency_id,  data_year, reported_flag, male_officer, male_civilian, male_total, female_officer, female_civilian, female_total, officer_rate, employee_rate, data_home, ddocname, did, ff_line_number, orig_format, pe_employee_id, extra) FROM '$MYPWD/PE.csv' WITH DELIMITER ',';
 
 
-DROP TABLE IF EXISTS reta_territories_csv;
-
-CREATE TABLE reta_territories_csv (
-year smallint NOT NULL,
-state_name text,
-state_abbr character varying(2),
-locality text,
-population bigint,
-violent_crime bigint,
-homicide bigint,
-rape_legacy bigint,
-rape_revised bigint,
-robbery bigint,
-aggravated_assault bigint,
-property_crime bigint,
-burglary bigint,
-larceny bigint,
-motor_vehicle_theft bigint,
-arson bigint,
-caveats text
-);
-
-\COPY reta_territories_csv FROM 'territories.csv' DELIMITER ',' HEADER CSV;
-
-DROP TABLE IF EXISTS reta_territories;
-
-CREATE TABLE reta_territories
-(
-  territory_id serial PRIMARY KEY NOT NULL,
-  year smallint NOT NULL,
-  state_id smallint REFERENCES ref_state(state_id),
-  state_abbr character varying(2),
-  locality text,
-  population bigint,
-  violent_crime bigint,
-  homicide bigint,
-  rape_legacy bigint,
-  rape_revised bigint,
-  robbery bigint,
-  aggravated_assault bigint,
-  property_crime bigint,
-  burglary bigint,
-  larceny bigint,
-  motor_vehicle_theft bigint,
-  arson bigint,
-  caveats text,
-  UNIQUE (year, state_id, locality)
-);
-
-INSERT INTO reta_territories(year, state_id, state_abbr, locality, population, violent_crime, homicide, rape_legacy, rape_revised, robbery, aggravated_assault, property_crime, burglary, larceny, motor_vehicle_theft, arson, caveats)
-SELECT
-ret.year,
-rs.state_id,
-ret.state_abbr,
-ret.locality,
-ret.population,
-ret.violent_crime,
-ret.homicide,
-ret.rape_legacy,
-ret.rape_revised,
-ret.robbery,
-ret.aggravated_assault,
-ret.property_crime,
-ret.burglary,
-ret.larceny,
-ret.motor_vehicle_theft,
-ret.arson,
-ret.caveats
-FROM reta_territories_csv ret
-JOIN ref_state rs ON rs.state_postal_abbr = ret.state_abbr;
-
-DROP TABLE reta_territories_csv;
-
-
+\COPY asr_estimated_csv FROM '$MYPWD/asr_estimated.csv' DELIMITER ',' HEADER CSV;
 -- You will have to run each step of this manually on the server
 DROP TABLE IF EXISTS reta_estimated_csv;
 
@@ -742,5 +669,80 @@ LEFT OUTER JOIN reta_estimated r ON r.year=c.year
 WHERE r.state_id IS NULL;
 
 DROP TABLE asr_estimated_csv;
+
+\COPY reta_territories_csv FROM '$MYPWD/territories.csv' DELIMITER ',' HEADER CSV;
+DROP TABLE IF EXISTS reta_territories_csv;
+
+CREATE TABLE reta_territories_csv (
+year smallint NOT NULL,
+state_name text,
+state_abbr character varying(2),
+locality text,
+population bigint,
+violent_crime bigint,
+homicide bigint,
+rape_legacy bigint,
+rape_revised bigint,
+robbery bigint,
+aggravated_assault bigint,
+property_crime bigint,
+burglary bigint,
+larceny bigint,
+motor_vehicle_theft bigint,
+arson bigint,
+caveats text
+);
+
+\COPY reta_territories_csv FROM 'territories.csv' DELIMITER ',' HEADER CSV;
+
+DROP TABLE IF EXISTS reta_territories;
+
+CREATE TABLE reta_territories
+(
+  territory_id serial PRIMARY KEY NOT NULL,
+  year smallint NOT NULL,
+  state_id smallint REFERENCES ref_state(state_id),
+  state_abbr character varying(2),
+  locality text,
+  population bigint,
+  violent_crime bigint,
+  homicide bigint,
+  rape_legacy bigint,
+  rape_revised bigint,
+  robbery bigint,
+  aggravated_assault bigint,
+  property_crime bigint,
+  burglary bigint,
+  larceny bigint,
+  motor_vehicle_theft bigint,
+  arson bigint,
+  caveats text,
+  UNIQUE (year, state_id, locality)
+);
+
+INSERT INTO reta_territories(year, state_id, state_abbr, locality, population, violent_crime, homicide, rape_legacy, rape_revised, robbery, aggravated_assault, property_crime, burglary, larceny, motor_vehicle_theft, arson, caveats)
+SELECT
+ret.year,
+rs.state_id,
+ret.state_abbr,
+ret.locality,
+ret.population,
+ret.violent_crime,
+ret.homicide,
+ret.rape_legacy,
+ret.rape_revised,
+ret.robbery,
+ret.aggravated_assault,
+ret.property_crime,
+ret.burglary,
+ret.larceny,
+ret.motor_vehicle_theft,
+ret.arson,
+ret.caveats
+FROM reta_territories_csv ret
+JOIN ref_state rs ON rs.state_postal_abbr = ret.state_abbr;
+
+DROP TABLE reta_territories_csv;
+
 
 "
