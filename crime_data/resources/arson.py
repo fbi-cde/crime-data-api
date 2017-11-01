2938c65fdabd6a0eaf6a9201a29f4605ea95ee6a
Jacob Wentz <jpwentz@ws05.ucr.development.cjis.gov>
2017-11-01 08:34:40 -0400

    Region Work Continued

--- crime_data/resources/arson.py
+++ crime_data/resources/arson.py
@@ -3,7 +3,7 @@ from crime_data.extensions import DEFAULT_MAX_AGE
 from flask.ext.cachecontrol import cache
 from sqlalchemy import func
 
-from crime_data.common import cdemodels, marshmallow_schemas
+from crime_data.common import cdemodels, marshmallow_schemas, lookupmodels
 from crime_data.common.base import CdeResource, tuning_page
 from crime_data.common.newmodels import ArsonSummary
 
@@ -15,14 +15,21 @@ class ArsonStateCounts(CdeResource):
     @use_args(marshmallow_schemas.ArgumentsSchema)
     @cache(max_age=DEFAULT_MAX_AGE, public=True)
     @tuning_page
-    def get(self, args, state_abbr=None):
+    def get(self, args, state_abbr=None, region_code=None):
         self.verify_api_key(args)
         query = ArsonSummary.query
 
-        if state_abbr is None:
-            query = query.filter(ArsonSummary.state_abbr == None)
-        else:
+        if state_abbr is not None:
             query = query.filter(func.lower(ArsonSummary.state_abbr) == func.lower(state_abbr))
+        elif region_code is not None:
+            states = lookupmodels.StateLK.get(region_code=region_code).all()
+            id_arr= []
+            [id_arr.append(state.state_abbr) for state in states]
+            print(id_arr)
+            query = query.filter(ArsonSummary.state_abbr.in_(id_arr))
+        else:
+            query = query.filter(ArsonSummary.state_abbr == None)
+
 
         query = query.filter(ArsonSummary.ori == None)
         query = query.filter(ArsonSummary.year != None)
