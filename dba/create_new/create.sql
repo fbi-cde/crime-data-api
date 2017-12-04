2d136ffecf79dba059b065b67d01a6c248f58859
David Hileman <dkhileman@fbi.gov>
2017-12-04 14:29:06 -0500

    Created views for police employment metrics

--- dba/create_new/create.sql
+++ dba/create_new/create.sql
@@ -325,7 +325,83 @@ UPDATE public.summarized_data sd
   SUM(sum_ars) as arson
                 from public.summarized_data GROUP BY ori,ncic_agency_name,agency_name_edit, agency_type_name, state_abbr,data_year;
 
+CREATE MATERIALIZED VIEW police_employment_nation AS
+  SELECT
+    data_year as data_year,
+    SUM(CASE WHEN month_num = 12 THEN population ELSE 0 END) as population,
+    SUM(male_officer) as male_officer_ct,
+    SUM(male_civilian) as male_civilian_ct,
+    SUM(male_total) as male_total_ct,
+    SUM(female_officer) as female_officer_ct,
+    SUM(female_civilian) as female_civilian_ct,
+    SUM(female_total) as female_total_ct,
+    SUM(male_officer)+SUM(female_officer) as officer_ct,
+    SUM(male_civilian)+SUM(female_civilian) as civilian_ct,
+    SUM(male_total)+SUM(female_total) as total_pe_ct,
+    ROUND(((SUM(male_total)+SUM(female_total))*1.0 / SUM(CASE WHEN month_num = 12 THEN population ELSE 0 END))*1000,2) as pe_ct_per_1000
+  FROM public.summarized_data
+  GROUP BY data_year;
 
+CREATE MATERIALIZED VIEW police_employment_region AS
+  SELECT
+    region_code as region_code,
+    region_name as region_name,
+    data_year as data_year,
+    SUM(CASE WHEN month_num = 12 THEN population ELSE 0 END) as population,
+    SUM(male_officer) as male_officer_ct,
+    SUM(male_civilian) as male_civilian_ct,
+    SUM(male_total) as male_total_ct,
+    SUM(female_officer) as female_officer_ct,
+    SUM(female_civilian) as female_civilian_ct,
+    SUM(female_total) as female_total_ct,
+    SUM(male_officer)+SUM(female_officer) as officer_ct,
+    SUM(male_civilian)+SUM(female_civilian) as civilian_ct,
+    SUM(male_total)+SUM(female_total) as total_pe_ct,
+    ROUND(((SUM(male_total)+SUM(female_total))*1.0 / NULLIF(SUM(CASE WHEN month_num = 12 THEN population ELSE 0 END),0))*1000,2) as pe_ct_per_1000
+  FROM public.summarized_data
+  GROUP BY region_code, region_name, data_year;
+
+CREATE MATERIALIZED VIEW police_employment_state AS
+  SELECT
+    state_id as state_id,
+    state_name as state_name,
+    state_abbr as state_abbr,
+    data_year as data_year,
+    SUM(CASE WHEN month_num = 12 THEN population ELSE 0 END) as population,
+    SUM(male_officer) as male_officer_ct,
+    SUM(male_civilian) as male_civilian_ct,
+    SUM(male_total) as male_total_ct,
+    SUM(female_officer) as female_officer_ct,
+    SUM(female_civilian) as female_civilian_ct,
+    SUM(female_total) as female_total_ct,
+    SUM(male_officer)+SUM(female_officer) as officer_ct,
+    SUM(male_civilian)+SUM(female_civilian) as civilian_ct,
+    SUM(male_total)+SUM(female_total) as total_pe_ct,
+    ROUND(((SUM(male_total)+SUM(female_total))*1.0 / NULLIF(SUM(CASE WHEN month_num = 12 THEN population ELSE 0 END),0))*1000,2) as pe_ct_per_1000
+  FROM public.summarized_data
+  GROUP BY state_id, state_name, state_abbr, data_year;
+
+CREATE MATERIALIZED VIEW police_employment_agency AS
+  SELECT
+    ori as ori,
+    ncic_agency_name as ncic_agency_name,
+    agency_name_edit as agency_name_edit,
+    agency_type_name as agency_type_name,
+    state_abbr as state_abbr,
+    data_year as data_year,
+    SUM(CASE WHEN month_num = 12 THEN population ELSE 0 END) as population,
+    SUM(male_officer) as male_officer_ct,
+    SUM(male_civilian) as male_civilian_ct,
+    SUM(male_total) as male_total_ct,
+    SUM(female_officer) as female_officer_ct,
+    SUM(female_civilian) as female_civilian_ct,
+    SUM(female_total) as female_total_ct,
+    SUM(male_officer)+SUM(female_officer) as officer_ct,
+    SUM(male_civilian)+SUM(female_civilian) as civilian_ct,
+    SUM(male_total)+SUM(female_total) as total_pe_ct,
+    ROUND(((SUM(male_total)+SUM(female_total))*1.0 / NULLIF(SUM(CASE WHEN month_num = 12 THEN population ELSE 0 END),0))*1000,2) as pe_ct_per_1000
+  FROM public.summarized_data
+  GROUP BY ori, ncic_agency_name, agency_name_edit, agency_type_name, state_abbr, data_year;
 
 -- Drops
 drop MATERIALIZED VIEW summarized_data_state;
@@ -333,6 +409,10 @@ drop MATERIALIZED VIEW  summarized_data_region;
 drop MATERIALIZED VIEW  summarized_data_national;
 drop MATERIALIZED VIEW summarized_data_agency;
 drop MATERIALIZED VIEW agencies;
+drop MATERIALIZED VIEW police_employment_nation;
+drop MATERIALIZED VIEW police_employment_region;
+drop MATERIALIZED VIEW police_employment_state;
+drop MATERIALIZED VIEW police_employment_agency;
 
 --Refreshes
 -- Drops
@@ -341,3 +421,7 @@ refresh MATERIALIZED VIEW  summarized_data_region;
 refresh MATERIALIZED VIEW  summarized_data_national;
 refresh MATERIALIZED VIEW summarized_data_agency;
 refresh MATERIALIZED VIEW agencies;
+refresh MATERIALIZED VIEW police_employment_nation;
+refresh MATERIALIZED VIEW police_employment_region;
+refresh MATERIALIZED VIEW police_employment_state;
+refresh MATERIALIZED VIEW police_employment_agency;
