19861b2ef7e6cbc3103f86cc73980c55a1dadb0b
Jacob Harris <jacob.harris@gsa.gov>
2017-12-07 16:27:59 -0500

    Added README and DB scripts to NIBRS bulk download files

---
+++ dba/nibrs_bulk_downloads/code_tables/sqlite_load.sql
@@ -0,0 +1,18 @@
+.mode csv
+.import 'nibrs_arrestee.csv' nibrs_arrestee
+.import 'nibrs_arrestee_weapon.csv' nibrs_arrestee_weapon
+.import 'nibrs_bias_motivation.csv' nibrs_bias_motivation
+.import 'nibrs_month.csv' nibrs_month
+.import 'nibrs_incident.csv' nibrs_incident
+.import 'nibrs_offender.csv' nibrs_offender
+.import 'nibrs_offense.csv' nibrs_offense
+.import 'nibrs_property.csv' nibrs_property
+.import 'nibrs_property_desc.csv' nibrs_property_desc
+.import 'nibrs_suspect_using.csv' nibrs_suspect_using
+.import 'nibrs_suspected_drug.csv' nibrs_suspected_drug
+.import 'nibrs_victim.csv' nibrs_victim
+.import 'nibrs_victim_circumstances.csv' nibrs_victim_circumstances
+.import 'nibrs_victim_injury.csv' nibrs_victim_injury
+.import 'nibrs_victim_offender_rel.csv' nibrs_victim_offender_rel
+.import 'nibrs_victim_offense.csv' nibrs_victim_offense
+.import 'nibrs_weapon.csv' nibrs_weapon
