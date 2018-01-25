e235bc6ef62c3382ab71dad700f1d0620497f29e
Jacob Wentz <jpwentz@ws05.ucr.development.cjis.gov>
2018-01-25 10:55:36 -0500

    Update to MVs

--- dba/create_new/leoka_assault_data_mv.sql
+++ dba/create_new/leoka_assault_data_mv.sql
@@ -85,200 +85,82 @@ JOIN (SELECT a.data_year,
  ) table9 ON table1.activity_id=table9.activity_id
  ORDER BY activity_id;
 
-CREATE MATERIALIZED VIEW public.leoka_assault_by_group_regional AS
-select table1.data_year, table1.region_code, table1.region_name, table1.activity_name, table1.activity_id,
-table1.group_1_actual_ct, table1.group_1_cleared_ct,
-table2.group_2_actual_ct, table2.group_2_cleared_ct,
-table3.group_3_actual_ct, table3.group_3_cleared_ct,
-table4.group_4_actual_ct, table4.group_4_cleared_ct,
-table5.group_5_actual_ct, table5.group_5_cleared_ct,
-table6.group_6_actual_ct, table6.group_6_cleared_ct,
-table7.group_7_actual_ct, table7.group_7_cleared_ct,
-table8.group_8_actual_ct, table8.group_8_cleared_ct,
-table9.group_9_actual_ct, table9.group_9_cleared_ct
-FROM
-(SELECT a.data_year,
-   b.region_code,
-   b.region_name,
-   a.activity_name,
-   a.activity_id,
-   sum(a.firearm_actual)+sum(a.knife_actual)+sum(a.hands_fists_feet_actual)+sum(a.other_actual) as group_1_actual_ct,
-   sum(a.cleared_count) as group_1_cleared_ct
- FROM public.leoka_assault_activity_assignemnt_data a JOIN public.agency_data b on  b.agency_id=a.agency_id
- WHERE b.population_group_code like '1%'
- GROUP BY a.data_year, b.region_code, b.region_name, a.activity_name, a.activity_id) table1
-JOIN (SELECT a.data_year,
-   a.activity_id,
-   sum(a.firearm_actual)+sum(a.knife_actual)+sum(a.hands_fists_feet_actual)+sum(a.other_actual) as group_2_actual_ct,
-   sum(a.cleared_count) as group_2_cleared_ct
- FROM public.leoka_assault_activity_assignemnt_data a JOIN public.agency_data b on  b.agency_id=a.agency_id
+ CREATE MATERIALIZED VIEW public.leoka_assault_by_group_regional_totals AS
+ select lc.data_year, lc.activity_name, lc.activity_id, a.region_name,a.region_code,a.population_group_code,
+ sum(lc.firearm_actual)+sum(lc.knife_actual)+sum(lc.hands_fists_feet_actual)+sum(lc.other_actual) as group_count,
+ sum(lc.cleared_count) as cleared_count
+ FROM public.leoka_assault_activity_assignemnt_data lc
+ JOIN public.agency_data a on a.agency_id=lc.agency_id
+ GROUP BY lc.data_year, a.region_code,a.region_name,lc.activity_name, lc.activity_id, a.population_group_code
+ ORDER BY lc.data_year, lc.activity_id;
 
- WHERE b.population_group_code = '2'
- GROUP BY a.data_year, b.region_code, b.region_name, a.activity_name, a.activity_id
- ) table2 ON table1.activity_id=table2.activity_id
- JOIN (SELECT a.data_year,
-   a.activity_name,
-   a.activity_id,
-   sum(a.firearm_actual)+sum(a.knife_actual)+sum(a.hands_fists_feet_actual)+sum(a.other_actual) as group_3_actual_ct,
-   sum(a.cleared_count) as group_3_cleared_ct
- FROM public.leoka_assault_activity_assignemnt_data a JOIN public.agency_data b on  b.agency_id=a.agency_id
- WHERE b.population_group_code = '3'
- GROUP BY a.data_year, b.region_code, b.region_name, a.activity_name, a.activity_id
- ) table3 ON table1.activity_id=table3.activity_id
- JOIN (SELECT a.data_year,
-   a.activity_id,
-   sum(a.firearm_actual)+sum(a.knife_actual)+sum(a.hands_fists_feet_actual)+sum(a.other_actual) as group_4_actual_ct,
-   sum(a.cleared_count) as group_4_cleared_ct
- FROM public.leoka_assault_activity_assignemnt_data a JOIN public.agency_data b on  b.agency_id=a.agency_id
 
- WHERE b.population_group_code = '4'
- GROUP BY a.data_year, b.region_code, b.region_name, a.activity_name, a.activity_id
- ) table4 ON table1.activity_id=table4.activity_id
- JOIN (SELECT a.data_year,
-   a.activity_id,
-   sum(a.firearm_actual)+sum(a.knife_actual)+sum(a.hands_fists_feet_actual)+sum(a.other_actual) as group_5_actual_ct,
-   sum(a.cleared_count) as group_5_cleared_ct
- FROM public.leoka_assault_activity_assignemnt_data a JOIN public.agency_data b on  b.agency_id=a.agency_id
+ CREATE MATERIALIZED VIEW public.leoka_assault_by_group_regional AS
+ SELECT
+    data_year,
+    region_code,
+    region_name,
+    activity_id,
+    sum(CASE WHEN population_group_code like '1%' then group_count else 0 end) as group_1_actual_ct,
+    sum(CASE WHEN population_group_code like '1%' then cleared_count else 0 end) as group_1_cleared_ct,
+    sum(CASE WHEN population_group_code like '2%' then group_count else 0 end) as group_2_actual_ct,
+    sum(CASE WHEN population_group_code like '2%' then cleared_count else 0 end) as group_2_cleared_ct,
+    sum(CASE WHEN population_group_code like '3%' then group_count else 0 end) as group_3_actual_ct,
+    sum(CASE WHEN population_group_code like '3%' then cleared_count else 0 end) as group_3_cleared_ct,
+    sum(CASE WHEN population_group_code like '4%' then group_count else 0 end) as group_4_actual_ct,
+    sum(CASE WHEN population_group_code like '4%' then cleared_count else 0 end) as group_4_cleared_ct,
+    sum(CASE WHEN population_group_code like '5%' then group_count else 0 end) as group_5_actual_ct,
+    sum(CASE WHEN population_group_code like '5%' then cleared_count else 0 end) as group_5_cleared_ct,
+    sum(CASE WHEN population_group_code like '6%' then group_count else 0 end) as group_6_actual_ct,
+    sum(CASE WHEN population_group_code like '6%' then cleared_count else 0 end) as group_6_cleared_ct,
+    sum(CASE WHEN population_group_code like '7%' then group_count else 0 end) as group_7_actual_ct,
+    sum(CASE WHEN population_group_code like '7%' then cleared_count else 0 end) as group_7_cleared_ct,
+    sum(CASE WHEN population_group_code like '8%' then group_count else 0 end) as group_8_actual_ct,
+    sum(CASE WHEN population_group_code like '8%' then cleared_count else 0 end) as group_8_cleared_ct,
+    sum(CASE WHEN population_group_code like '9%' then group_count else 0 end) as group_9_actual_ct,
+    sum(CASE WHEN population_group_code like '9%' then cleared_count else 0 end) as group_9_cleared_ct
+ from public.leoka_assault_by_group_regional_totals
+ group by data_year, region_code,region_name,activity_id
+ ORDER BY data_year, activity_id,region_code;
 
- WHERE b.population_group_code = '4'
- GROUP BY a.data_year, b.region_code, b.region_name, a.activity_name, a.activity_id
- ) table5 ON table1.activity_id=table5.activity_id
- JOIN (SELECT a.data_year,
-   a.activity_id,
-   sum(a.firearm_actual)+sum(a.knife_actual)+sum(a.hands_fists_feet_actual)+sum(a.other_actual) as group_6_actual_ct,
-   sum(a.cleared_count) as group_6_cleared_ct
- FROM public.leoka_assault_activity_assignemnt_data a JOIN public.agency_data b on  b.agency_id=a.agency_id
 
- WHERE b.population_group_code = '6'
- GROUP BY a.data_year, b.region_code, b.region_name, a.activity_name, a.activity_id
- ) table6 ON table1.activity_id=table6.activity_id
- JOIN (SELECT a.data_year,
-   a.activity_id,
-   sum(a.firearm_actual)+sum(a.knife_actual)+sum(a.hands_fists_feet_actual)+sum(a.other_actual) as group_7_actual_ct,
-   sum(a.cleared_count) as group_7_cleared_ct
- FROM public.leoka_assault_activity_assignemnt_data a JOIN public.agency_data b on  b.agency_id=a.agency_id
+ CREATE MATERIALIZED VIEW public.leoka_assault_by_group_state_totals AS
+ select lc.data_year, lc.activity_name, lc.activity_id, a.state_abbr,a.state_id,a.population_group_code,
+ sum(lc.firearm_actual)+sum(lc.knife_actual)+sum(lc.hands_fists_feet_actual)+sum(lc.other_actual) as group_count,
+ sum(lc.cleared_count) as cleared_count
+ FROM public.leoka_assault_activity_assignemnt_data lc
+ JOIN public.agency_data a on a.agency_id=lc.agency_id
+ GROUP BY lc.data_year, a.state_abbr,a.state_id,lc.activity_name, lc.activity_id, a.population_group_code
+ ORDER BY lc.data_year, lc.activity_id;
 
- WHERE b.population_group_code = '7'
- GROUP BY a.data_year, b.region_code, b.region_name, a.activity_name, a.activity_id
- ) table7 ON table1.activity_id=table7.activity_id
- JOIN (SELECT a.data_year,
-   a.activity_id,
-   sum(a.firearm_actual)+sum(a.knife_actual)+sum(a.hands_fists_feet_actual)+sum(a.other_actual) as group_8_actual_ct,
-   sum(a.cleared_count) as group_8_cleared_ct
- FROM public.leoka_assault_activity_assignemnt_data a JOIN public.agency_data b on  b.agency_id=a.agency_id
 
- WHERE b.population_group_code like '8%'
- GROUP BY a.data_year, b.region_code, b.region_name, a.activity_name, a.activity_id
- ) table8 ON table1.activity_id=table8.activity_id
- JOIN (SELECT a.data_year,
-   a.activity_id,
-   sum(a.firearm_actual)+sum(a.knife_actual)+sum(a.hands_fists_feet_actual)+sum(a.other_actual) as group_9_actual_ct,
-   sum(a.cleared_count) as group_9_cleared_ct
- FROM public.leoka_assault_activity_assignemnt_data a JOIN public.agency_data b on  b.agency_id=a.agency_id
-
- WHERE b.population_group_code like '9%'
- GROUP BY a.data_year, b.region_code, b.region_name, a.activity_name, a.activity_id
- ) table9 ON table1.activity_id=table9.activity_id
- ORDER BY activity_id;
-
-CREATE MATERIALIZED VIEW public.leoka_assault_by_group_state AS
-select table1.data_year, table1.state_abbr, table1.activity_name, table1.activity_id,
-table1.group_1_actual_ct, table1.group_1_cleared_ct,
-table2.group_2_actual_ct, table2.group_2_cleared_ct,
-table3.group_3_actual_ct, table3.group_3_cleared_ct,
-table4.group_4_actual_ct, table4.group_4_cleared_ct,
-table5.group_5_actual_ct, table5.group_5_cleared_ct,
-table6.group_6_actual_ct, table6.group_6_cleared_ct,
-table7.group_7_actual_ct, table7.group_7_cleared_ct,
-table8.group_8_actual_ct, table8.group_8_cleared_ct,
-table9.group_9_actual_ct, table9.group_9_cleared_ct
-FROM
-(SELECT a.data_year,
-   b.state_abbr,
-   b.state_id,
-   a.activity_name,
-   a.activity_id,
-   sum(a.firearm_actual)+sum(a.knife_actual)+sum(a.hands_fists_feet_actual)+sum(a.other_actual) as group_1_actual_ct,
-   sum(a.cleared_count) as group_1_cleared_ct
- FROM public.leoka_assault_activity_assignemnt_data a JOIN public.agency_data b on b.agency_id=a.agency_id
-
- WHERE b.population_group_code like '1%'
- GROUP BY a.data_year, b.state_abbr, b.state_id, a.activity_name, a.activity_id) table1
-JOIN (SELECT a.data_year,
-   a.activity_id,
-   sum(a.firearm_actual)+sum(a.knife_actual)+sum(a.hands_fists_feet_actual)+sum(a.other_actual) as group_2_actual_ct,
-   sum(a.cleared_count) as group_2_cleared_ct
- FROM public.leoka_assault_activity_assignemnt_data a JOIN public.agency_data b on  b.agency_id=a.agency_id
-
- WHERE b.population_group_code = '2'
- GROUP BY a.data_year, b.state_abbr, a.activity_name, a.activity_id
- ) table2 ON table1.activity_id=table2.activity_id
- JOIN (SELECT a.data_year,
-   a.activity_name,
-   a.activity_id,
-   sum(a.firearm_actual)+sum(a.knife_actual)+sum(a.hands_fists_feet_actual)+sum(a.other_actual) as group_3_actual_ct,
-   sum(a.cleared_count) as group_3_cleared_ct
- FROM public.leoka_assault_activity_assignemnt_data a JOIN public.agency_data b on  b.agency_id=a.agency_id
-
- WHERE b.population_group_code = '3'
- GROUP BY a.data_year, b.state_abbr, a.activity_name, a.activity_id
- ) table3 ON table1.activity_id=table3.activity_id
- JOIN (SELECT a.data_year,
-   a.activity_id,
-   sum(a.firearm_actual)+sum(a.knife_actual)+sum(a.hands_fists_feet_actual)+sum(a.other_actual) as group_4_actual_ct,
-   sum(a.cleared_count) as group_4_cleared_ct
- FROM public.leoka_assault_activity_assignemnt_data a JOIN public.agency_data b on  b.agency_id=a.agency_id
-
- WHERE b.population_group_code = '4'
- GROUP BY a.data_year, b.state_abbr, a.activity_name, a.activity_id
- ) table4 ON table1.activity_id=table4.activity_id
- JOIN (SELECT a.data_year,
-   a.activity_id,
-   sum(a.firearm_actual)+sum(a.knife_actual)+sum(a.hands_fists_feet_actual)+sum(a.other_actual) as group_5_actual_ct,
-   sum(a.cleared_count) as group_5_cleared_ct
- FROM public.leoka_assault_activity_assignemnt_data a JOIN public.agency_data b on  b.agency_id=a.agency_id
-
- WHERE b.population_group_code = '4'
- GROUP BY a.data_year, b.state_abbr, a.activity_name, a.activity_id
- ) table5 ON table1.activity_id=table5.activity_id
- JOIN (SELECT a.data_year,
-   a.activity_id,
-   sum(a.firearm_actual)+sum(a.knife_actual)+sum(a.hands_fists_feet_actual)+sum(a.other_actual) as group_6_actual_ct,
-   sum(a.cleared_count) as group_6_cleared_ct
- FROM public.leoka_assault_activity_assignemnt_data a JOIN public.agency_data b on  b.agency_id=a.agency_id
-
- WHERE b.population_group_code = '6'
- GROUP BY a.data_year, b.state_abbr, a.activity_name, a.activity_id
- ) table6 ON table1.activity_id=table6.activity_id
- JOIN (SELECT a.data_year,
-   a.activity_id,
-   sum(a.firearm_actual)+sum(a.knife_actual)+sum(a.hands_fists_feet_actual)+sum(a.other_actual) as group_7_actual_ct,
-   sum(a.cleared_count) as group_7_cleared_ct
- FROM public.leoka_assault_activity_assignemnt_data a JOIN public.agency_data b on  b.agency_id=a.agency_id
-
- WHERE b.population_group_code = '7'
- GROUP BY a.data_year, b.state_abbr, a.activity_name, a.activity_id
- ) table7 ON table1.activity_id=table7.activity_id
- JOIN (SELECT a.data_year,
-   a.activity_id,
-   sum(a.firearm_actual)+sum(a.knife_actual)+sum(a.hands_fists_feet_actual)+sum(a.other_actual) as group_8_actual_ct,
-   sum(a.cleared_count) as group_8_cleared_ct
- FROM public.leoka_assault_activity_assignemnt_data a JOIN public.agency_data b on  b.agency_id=a.agency_id
-
- WHERE b.population_group_code like '8%'
- GROUP BY a.data_year, b.state_abbr, a.activity_name, a.activity_id
- ) table8 ON table1.activity_id=table8.activity_id
- JOIN (SELECT a.data_year,
-   a.activity_id,
-   sum(a.firearm_actual)+sum(a.knife_actual)+sum(a.hands_fists_feet_actual)+sum(a.other_actual) as group_9_actual_ct,
-   sum(a.cleared_count) as group_9_cleared_ct
- FROM public.leoka_assault_activity_assignemnt_data a
- JOIN public.agency_data b on b.agency_id=a.agency_id
- WHERE b.population_group_code like '9%'
- GROUP BY a.data_year, b.state_abbr,b.state_id, a.activity_name, a.activity_id
- ) table9 ON table1.activity_id=table9.activity_id
- ORDER BY activity_id;
-
+ CREATE MATERIALIZED VIEW public.leoka_assault_by_group_state AS
+ SELECT
+    data_year,
+    state_abbr,
+    state_id,
+    activity_id,
+    sum(CASE WHEN population_group_code like '1%' then group_count else 0 end) as group_1_actual_ct,
+    sum(CASE WHEN population_group_code like '1%' then cleared_count else 0 end) as group_1_cleared_ct,
+    sum(CASE WHEN population_group_code like '2%' then group_count else 0 end) as group_2_actual_ct,
+    sum(CASE WHEN population_group_code like '2%' then cleared_count else 0 end) as group_2_cleared_ct,
+    sum(CASE WHEN population_group_code like '3%' then group_count else 0 end) as group_3_actual_ct,
+    sum(CASE WHEN population_group_code like '3%' then cleared_count else 0 end) as group_3_cleared_ct,
+    sum(CASE WHEN population_group_code like '4%' then group_count else 0 end) as group_4_actual_ct,
+    sum(CASE WHEN population_group_code like '4%' then cleared_count else 0 end) as group_4_cleared_ct,
+    sum(CASE WHEN population_group_code like '5%' then group_count else 0 end) as group_5_actual_ct,
+    sum(CASE WHEN population_group_code like '5%' then cleared_count else 0 end) as group_5_cleared_ct,
+    sum(CASE WHEN population_group_code like '6%' then group_count else 0 end) as group_6_actual_ct,
+    sum(CASE WHEN population_group_code like '6%' then cleared_count else 0 end) as group_6_cleared_ct,
+    sum(CASE WHEN population_group_code like '7%' then group_count else 0 end) as group_7_actual_ct,
+    sum(CASE WHEN population_group_code like '7%' then cleared_count else 0 end) as group_7_cleared_ct,
+    sum(CASE WHEN population_group_code like '8%' then group_count else 0 end) as group_8_actual_ct,
+    sum(CASE WHEN population_group_code like '8%' then cleared_count else 0 end) as group_8_cleared_ct,
+    sum(CASE WHEN population_group_code like '9%' then group_count else 0 end) as group_9_actual_ct,
+    sum(CASE WHEN population_group_code like '9%' then cleared_count else 0 end) as group_9_cleared_ct
+ from public.leoka_assault_by_group_state_totals
+ group by data_year, state_abbr,state_id,activity_id
+ ORDER BY data_year, activity_id;
 
  CREATE MATERIALIZED VIEW public.leoka_assault_by_assign_dist_national AS
  SELECT data_year,
