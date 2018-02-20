bf7d80cb505bbd149c51b4664565c78b51b416d4
Jacob Wentz <jpwentz@ws05.ucr.development.cjis.gov>
2018-02-20 12:23:25 -0500

    Update to Summarized MVs

--- dba/create_new/summarized_data_mv.sql
+++ dba/create_new/summarized_data_mv.sql
@@ -1,10 +1,10 @@
 CREATE MATERIALIZED VIEW summarized_data_agency AS
 select ori,  data_year as data_year,
    SUM(sum_hom_cleared) as homicide_cleared,
-   (SUM(sum_rpe_ns_leg_cleared)+SUM(sum_rpe_frc_leg_cleared)) as rape_legacy_cleared,
+   (COALESCE(SUM(sum_rpe_ns_leg_cleared),0)+COALESCE(SUM(sum_rpe_frc_leg_cleared),0)) as rape_legacy_cleared,
   (SUM(sum_rpe_ns_cleared)+SUM(sum_rpe_frc_cleared)+SUM(sum_rpe_att_cleared)) as rape_cleared,
   (SUM(sum_rob_ns_cleared)+SUM(sum_rob_gun_cleared)+SUM(sum_rob_cut_cleared)+SUM(sum_rob_oth_cleared)+SUM(sum_rob_hff_cleared)) as robbery_cleared,
-  (SUM(sum_ass_ns_cleared)+SUM(sum_ass_gun_cleared)+SUM(sum_ass_cut_cleared)+SUM(sum_ass_hff_cleared)+SUM(sum_hom_cleared)+SUM(sum_rpe_ns_leg_cleared)+SUM(sum_rpe_frc_leg_cleared)+SUM(sum_rpe_ns_cleared)+SUM(sum_rpe_frc_cleared)+SUM(sum_rpe_att_cleared)+SUM(sum_rob_ns_cleared)+SUM(sum_rob_gun_cleared)+SUM(sum_rob_cut_cleared)+SUM(sum_rob_oth_cleared)+SUM(sum_rob_hff_cleared)) as violent_crime_cleared,
+  (SUM(sum_ass_ns_cleared)+SUM(sum_ass_gun_cleared)+SUM(sum_ass_cut_cleared)+SUM(sum_ass_hff_cleared)+SUM(sum_hom_cleared)+COALESCE(SUM(sum_rpe_ns_leg_cleared),0)+COALESCE(SUM(sum_rpe_frc_leg_cleared),0)+SUM(sum_rpe_ns_cleared)+SUM(sum_rpe_frc_cleared)+SUM(sum_rpe_att_cleared)+SUM(sum_rob_ns_cleared)+SUM(sum_rob_gun_cleared)+SUM(sum_rob_cut_cleared)+SUM(sum_rob_oth_cleared)+SUM(sum_rob_hff_cleared)) as violent_crime_cleared,
   (SUM(sum_ass_ns_cleared)+SUM(sum_ass_gun_cleared)+SUM(sum_ass_cut_cleared)+SUM(sum_ass_hff_cleared)) as aggravated_assault_cleared,
   (SUM(sum_brg_ns_cleared)+SUM(sum_brg_feo_cleared)+SUM(sum_brg_ueo_cleared)+SUM(sum_brg_afe_cleared)) as burglary_cleared,
   SUM(sum_lar_tft_cleared) as larceny_cleared,
@@ -13,10 +13,10 @@ select ori,  data_year as data_year,
   (SUM(sum_ht_ns_cleared)+SUM(sum_ht_sex_cleared)+SUM(sum_ht_srv_cleared)) as human_trafficing_cleared,
   SUM(sum_ars_cleared) as arson_cleared,
   SUM(sum_hom_actual) as homicide_actual,
-  (SUM(sum_rpe_ns_leg_actual)+SUM(sum_rpe_frc_leg_actual)) as rape_legacy_actual,
+  (COALESCE(SUM(sum_rpe_ns_leg_actual),0)+COALESCE(SUM(sum_rpe_frc_leg_actual),0)) as rape_legacy_actual,
   (SUM(sum_rpe_ns_actual)+SUM(sum_rpe_frc_actual)+SUM(sum_rpe_att_actual)) as rape_actual,
   (SUM(sum_rob_ns_actual)+SUM(sum_rob_gun_actual)+SUM(sum_rob_cut_actual)+SUM(sum_rob_oth_actual)+SUM(sum_rob_hff_actual)) as robbery_actual,
-  (SUM(sum_ass_ns_actual)+SUM(sum_ass_gun_actual)+SUM(sum_ass_cut_actual)+SUM(sum_ass_hff_actual)+SUM(sum_hom_actual)+SUM(sum_rpe_ns_leg_actual)+SUM(sum_rpe_frc_leg_actual)+SUM(sum_rpe_ns_actual)+SUM(sum_rpe_frc_actual)+SUM(sum_rpe_att_actual)+SUM(sum_rob_ns_actual)+SUM(sum_rob_gun_actual)+SUM(sum_rob_cut_actual)+SUM(sum_rob_oth_actual)+SUM(sum_rob_hff_actual)) as violent_crime_actual,
+  (SUM(sum_ass_ns_actual)+SUM(sum_ass_gun_actual)+SUM(sum_ass_cut_actual)+SUM(sum_ass_hff_actual)+SUM(sum_hom_actual)+COALESCE(SUM(sum_rpe_ns_leg_actual),0)+COALESCE(SUM(sum_rpe_frc_leg_actual),0)+SUM(sum_rpe_ns_actual)+SUM(sum_rpe_frc_actual)+SUM(sum_rpe_att_actual)+SUM(sum_rob_ns_actual)+SUM(sum_rob_gun_actual)+SUM(sum_rob_cut_actual)+SUM(sum_rob_oth_actual)+SUM(sum_rob_hff_actual)) as violent_crime_actual,
   (SUM(sum_ass_ns_actual)+SUM(sum_ass_gun_actual)+SUM(sum_ass_cut_actual)+SUM(sum_ass_hff_actual)) as aggravated_assault_actual,
   (SUM(sum_brg_ns_actual)+SUM(sum_brg_feo_actual)+SUM(sum_brg_ueo_actual)+SUM(sum_brg_afe_actual)) as burglary_actual,
    SUM(sum_lar_tft_actual) as larceny_actual,
@@ -29,85 +29,85 @@ from public.summarized_data GROUP BY ori, data_year;
 CREATE MATERIALIZED VIEW summarized_data_agency_homicide AS
 select ori,  data_year as data_year,
 format('homicide') as offense,
-SUM(homicide_cleared) as cleared,
-SUM(homicide_actual) as actual
+CAST(coalesce(SUM(homicide_cleared)) as INTEGER) as cleared,
+CAST(coalesce(SUM(homicide_actual)) as INTEGER) as actual
 from public.summarized_data_agency GROUP BY ori, data_year;
 
 CREATE MATERIALIZED VIEW summarized_data_agency_rape AS
 select ori,  data_year as data_year,
 format('rape') as offense,
-SUM(rape_cleared) as cleared,
-SUM(rape_actual) as actual
+CAST(coalesce(SUM(rape_cleared)) as INTEGER) as cleared,
+CAST(coalesce(SUM(rape_actual)) as INTEGER) as actual
 from public.summarized_data_agency GROUP BY ori, data_year;
 
 CREATE MATERIALIZED VIEW summarized_data_agency_rape_leg AS
 select ori,  data_year as data_year,
 format('rape-legacy') as offense,
-SUM(rape_legacy_cleared) as cleared,
-SUM(rape_legacy_actual) as actual
+CAST(coalesce(SUM(rape_legacy_cleared)) as INTEGER) as cleared,
+CAST(coalesce(SUM(rape_legacy_actual)) as INTEGER) as actual
 from public.summarized_data_agency GROUP BY ori, data_year;
 
 CREATE MATERIALIZED VIEW summarized_data_agency_robbery AS
 select ori,  data_year as data_year,
 format('robbery') as offense,
-SUM(robbery_cleared) as cleared,
-SUM(robbery_actual) as actual
+CAST(coalesce(SUM(robbery_cleared)) as INTEGER) as cleared,
+CAST(coalesce(SUM(robbery_actual)) as INTEGER) as actual
 from public.summarized_data_agency GROUP BY ori, data_year;
 
 CREATE MATERIALIZED VIEW summarized_data_agency_aggravated AS
 select ori,  data_year as data_year,
 format('aggravated-assault') as offense,
-SUM(aggravated_assault_cleared) as cleared,
-SUM(aggravated_assault_actual) as actual
+CAST(coalesce(SUM(aggravated_assault_cleared)) as INTEGER) as cleared,
+CAST(coalesce(SUM(aggravated_assault_actual)) as INTEGER) as actual
 from public.summarized_data_agency GROUP BY ori, data_year;
 
 CREATE MATERIALIZED VIEW summarized_data_agency_mvt AS
 select ori,  data_year as data_year,
 format('motor-vehicle-theft') as offense,
-SUM(motor_vehicle_theft_cleared) as cleared,
-SUM(motor_vehicle_theft_actual) as actual
+CAST(coalesce(SUM(motor_vehicle_theft_cleared)) as INTEGER) as cleared,
+CAST(coalesce(SUM(motor_vehicle_theft_actual)) as INTEGER) as actual
 from public.summarized_data_agency GROUP BY ori, data_year;
 
 CREATE MATERIALIZED VIEW summarized_data_agency_buglary AS
 select ori,  data_year as data_year,
 format('buglary') as offense,
-SUM(burglary_cleared) as cleared,
-SUM(burglary_actual) as actual
+CAST(coalesce(SUM(burglary_cleared)) as INTEGER) as cleared,
+CAST(coalesce(SUM(burglary_actual)) as INTEGER) as actual
 from public.summarized_data_agency GROUP BY ori, data_year;
 
 CREATE MATERIALIZED VIEW summarized_data_agency_ht AS
 select ori,  data_year as data_year,
 format('human-trafficing') as offense,
-SUM(human_trafficing_cleared) as cleared,
-SUM(human_trafficing_actual) as actual
+CAST(coalesce(SUM(human_trafficing_cleared)) as INTEGER) as cleared,
+CAST(coalesce(SUM(human_trafficing_actual)) as INTEGER) as actual
 from public.summarized_data_agency GROUP BY ori, data_year;
 
 CREATE MATERIALIZED VIEW summarized_data_agency_vc AS
 select ori,  data_year as data_year,
 format('violent-crime') as offense,
-SUM(violent_crime_cleared) as cleared,
-SUM(violent_crime_actual) as actual
+CAST(coalesce(SUM(violent_crime_cleared)) as INTEGER) as cleared,
+CAST(coalesce(SUM(violent_crime_actual)) as INTEGER) as actual
 from public.summarized_data_agency GROUP BY ori, data_year;
 
 CREATE MATERIALIZED VIEW summarized_data_agency_pc AS
 select ori,  data_year as data_year,
 format('property-crime') as offense,
-SUM(property_crime_cleared) as cleared,
-SUM(property_crime_actual) as actual
+CAST(coalesce(SUM(property_crime_cleared)) as INTEGER) as cleared,
+CAST(coalesce(SUM(property_crime_actual)) as INTEGER) as actual
 from public.summarized_data_agency GROUP BY ori, data_year;
 
 CREATE MATERIALIZED VIEW summarized_data_agency_arson AS
 select ori,  data_year as data_year,
 format('arson') as offense,
-SUM(arson_cleared) as cleared,
-SUM(arson_actual) as actual
+CAST(coalesce(SUM(arson_cleared)) as INTEGER) as cleared,
+CAST(coalesce(SUM(arson_actual)) as INTEGER) as actual
 from public.summarized_data_agency GROUP BY ori, data_year;
 
 CREATE MATERIALIZED VIEW summarized_data_agency_larceny AS
 select ori,  data_year as data_year,
 format('larceny') as offense,
-SUM(larceny_cleared) as cleared,
-SUM(larceny_actual) as actual
+CAST(coalesce(SUM(larceny_cleared)) as INTEGER) as cleared,
+CAST(coalesce(SUM(larceny_actual)) as INTEGER) as actual
 from public.summarized_data_agency GROUP BY ori, data_year;
 
 CREATE MATERIALIZED VIEW summarized_data_agency_combined AS
